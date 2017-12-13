1536353f73f5036f22fcc955d25d00968376f396
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-12-13 08:50:51 +0800

    sidebar styling

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,12 +1,34 @@
 <!-- Sidebar Holder -->
 <nav id="sidebar">
 	<div class="sidebar-header">
-		<h3>Bootstrap Sidebar</h3>
-		<strong>BS</strong>
+		<a href="{{ route('admin') }}"> 
+			<h3>Mnemonic</h3>
+			<strong>MN</strong>
+		</a>
 	</div>
 
 	<ul class="list-unstyled components">
-		<li class="active">
+		<li class="active"><a href="{{ route('admin') }}">Dashboard</a></li>
+		<li class="header">Website</li>
+		<li>
+			<a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false">
+				<i class="glyphicon glyphicon-duplicate"></i>
+				Pages
+			</a>
+			<ul class="collapse list-unstyled" id="pageSubmenu">
+					<li>
+						<a href="#">Page 1</a>
+					</li>
+					<li>
+						<a href="#">Page 2</a>
+					</li>
+					<li>
+						<a href="#">Page 3</a>
+					</li>
+			</ul>
+		</li>
+
+		<li>
 			<a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">
 				<i class="glyphicon glyphicon-home"></i>
 				Home
@@ -28,21 +50,7 @@
 				<i class="glyphicon glyphicon-briefcase"></i>
 				About
 			</a>
-			<a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false">
-				<i class="glyphicon glyphicon-duplicate"></i>
-				Pages
-			</a>
-			<ul class="collapse list-unstyled" id="pageSubmenu">
-				<li>
-					<a href="#">Page 1</a>
-				</li>
-				<li>
-					<a href="#">Page 2</a>
-				</li>
-				<li>
-					<a href="#">Page 3</a>
-				</li>
-			</ul>
+			
 		</li>
 		<li>
 			<a href="#">
--- resources/views/layouts/submaster.blade.php
+++ resources/views/layouts/submaster.blade.php
@@ -17,58 +17,32 @@
 
 	<body>
 
-
 		<div class="wrapper">
 			@include('layouts.sidebar')
 
 			<!-- Page Content Holder -->
             <div class="container">
                 <div id="content">
-
                     @include('layouts.header')
-
-                    {{--  <h2>Collapsible Sidebar Using Bootstrap 3</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-                    <div class="line"></div>
-
-                    <h2>Lorem Ipsum Dolor</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-                    <div class="line"></div>
-
-                    <h2>Lorem Ipsum Dolor</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-                    <div class="line"></div>
-
-                    <h3>Lorem Ipsum Dolor</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>  --}}
                     @yield('content')
                 </div>
-            
             </div>
 		</div>
 
-		<!-- Scripts -->
+		{{--  Asset Scripts  --}}
 		<script src="{{ asset('js/main.js') }}"></script>
 
+        {{--  General scripts  --}}
+        <script>
+            $(document).ready(function () {
+
+                $('#sidebarCollapse').on('click', function () {
+                    $('#sidebar').toggleClass('active');
+                });
+
+            });
+        </script>
+        {{--  page-specific scripts  --}}
 		@yield('script')
 	</body>
 
