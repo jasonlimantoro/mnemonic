d18f3b56a94383b9f19247fba69aa1fb079b62b7
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-12-13 12:02:08 +0800

    fix relationship

--- app/Post.php
+++ app/Post.php
@@ -5,7 +5,7 @@ namespace App;
 
 class Post extends Model
 {
-    public function user() {
-        $this->belongsTo(User::class);
+    public function user() { // $post->user->name
+        return $this->belongsTo(User::class);
     }
 }
