b4b028fcfbb942f1468a278618e2ceba32002609
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-12-19 17:40:04 +0700

    using the CarouselFilter to clean CarouselController

--- app/Http/Controllers/CarouselController.php
+++ app/Http/Controllers/CarouselController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
+use App\Filters\CarouselFilter;
 
 class CarouselController extends Controller
 {
     public function showCarouselForm () {
         // All uploaded images
-        $images = $files = \Storage::disk('uploads')->files('/uploads/carousel');
+        $images = \Storage::disk('uploads')->files('/uploads/carousel');
 
         return view('backend.website.carousel-form', compact('images'));
     }
@@ -24,24 +25,12 @@ class CarouselController extends Controller
         $imageRequest = $request->file('image');
         $fileName = $imageRequest->getClientOriginalName();
         $fileDestination = public_path('uploads/carousel/' . $fileName);
+        
+        // create an Image instance
+        $img = Image::make($imageRequest);
 
-        // Store it in 'uploads' disk (see app/filesystems.php)
-        // $path = \Storage::disk('uploads')
-        //             ->putFileAs(
-        //                 'uploads/carousel', 
-        //                 $imageRequest,
-        //                 $fileName
-        //             );
-
-        // Create a Canvas
-        $canvas = Image::canvas(2000, 1000);
-        // Resize the image given constraints
-        $img = Image::make($imageRequest)->resize(2000, 1000, function($constraint){
-            $constraint->aspectRatio();
-            $constraint->upsize();
-        });
-        // Insert the image to the canvas and save it
-        $canvas->insert($img, 'center')->save($fileDestination);
+        // applyFilter CarouselFilter
+        $img->filter(new CarouselFilter($fileDestination))->save($fileDestination);
 
         \Session::flash('success_msg', 'Image is successfully uploaded!');
         return back();
