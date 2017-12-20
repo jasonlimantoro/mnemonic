87533e59f96fc44857e90fddaf84b01c3a9de464
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-12-20 12:15:30 +0700

    define a post factory and use it in the seeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Post::class, function (Faker $faker) {
+    return [
+        'user_id' => $faker->numberBetween($min = 1, $max = 2),
+        'page_id' => $faker->numberBetween($min = 1, $max = 2),
+        'title' => $faker->sentence(),
+        'body'=> $faker->paragraph($nbSentences = 4, $variableNbSentences = true),
+        'created_at' => $faker->dateTimeBetween($startDate = '-10 years', $endDate = '-2 years', $timezone = 'Asia/Singapore'),
+        'updated_at' => $faker->dateTimeBetween($startDate = '-2 years', $endDate = 'now', $timezone = 'Asia/Singapore')
+    ];
+});
