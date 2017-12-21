8cb99f756d98fcce89dba2c205849ef9cb6c63d7
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-12-21 18:25:32 +0700

    clean up

--- resources/views/layouts/show-all-post-btn.blade.php
+++ resources/views/layouts/show-all-post-btn.blade.php
@@ -1,7 +1,3 @@
-@php
-
-@endphp
-
 <a href="{{ route('pages.show', ['page' => $page->id]) }}" class="btn btn-danger">
     <i class="fa fa-angle-left"></i>
     Show All Posts in <strong>{{ $page->title }} </strong>
