88cdd648d8313243bfc1f0e7ed5895020a27eeb2
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-12-21 13:25:38 +0700

    caption is nullable

--- database/migrations/2017_12_19_170427_create_carousel_images_table.php
+++ database/migrations/2017_12_19_170427_create_carousel_images_table.php
@@ -16,7 +16,7 @@ class CreateCarouselImagesTable extends Migration
         Schema::create('carousel_images', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('carousel_id');
-            $table->text('caption');
+            $table->text('caption')->nullable();
             $table->string('file_name');
             $table->string('url_asset');
             $table->string('url_cache');
