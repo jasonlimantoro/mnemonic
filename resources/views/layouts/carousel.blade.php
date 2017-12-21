996b367650486973c9da3dffb3d7bcd5a93596d2
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-12-21 22:31:49 +0700

    if carouselImages are empty, carousel is not displayed

--- resources/views/layouts/carousel.blade.php
+++ resources/views/layouts/carousel.blade.php
@@ -10,12 +10,17 @@
 	</div>
 
 	<!-- Controls -->
-	<a class="left carousel-control" href="#{{ $carouselId }}" role="button" data-slide="prev">
-		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-		<span class="sr-only">Previous</span>
-	</a>
-	<a class="right carousel-control" href="#{{ $carouselId }}" role="button" data-slide="next">
-		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-		<span class="sr-only">Next</span>
-	</a>
+	<div class="control">
+
+		{{ $carouselControls }}
+		{{--  <a class="left carousel-control" href="#{{ $carouselId }}" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#{{ $carouselId }}" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>  --}}
+	</div>
+	
 </div>
