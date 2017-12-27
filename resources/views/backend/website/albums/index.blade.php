386599c71f9803c82bed297153441eda583126eb
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-12-28 00:03:19 +0100

    uncategorized album cannot be edited

--- resources/views/backend/website/albums/index.blade.php
+++ resources/views/backend/website/albums/index.blade.php
@@ -13,49 +13,54 @@
             <tr>
                 <td>
                     <a href="{{ route('albums.show', ['album' => $album->id ]) }}">
-                        {{ $album->name }} 
+                         {{ $album->name }} 
                     </a>
                 </td>
                 
                 <td>{{ $album->description }}</td>
-                <td class="text-center">
-                    <div>
-                        <a 
-                            href="{{ route('albums.show', ['album' => $album->id ]) }}" 
-                            id="ShowIcon" 
-                            class="__react-root" 
-                            role="button"
-                            data-toggle="tooltip"
-                            title="See info about this album"
-                            data-placement="top"
-                            >
-                        </a>
-                    </div>
-                    <div>
-                        <a 
-                            href="#" 
-                            id="EditIcon" 
-                            class="__react-root" 
-                            role="button"
-                            data-toggle="tooltip"
-                            title="Edit this album"
-                            data-placement="top"
-                            >
-                        </a>
-                    </div>
-                    <div>
-                        <a 
-                            href="#" 
-                            id="DeleteIcon" 
-                            class="__react-root" 
-                            role="button"
-                            data-toggle="tooltip"
-                            title="Delete this album"
-                            data-placement="top"
-                            >
-                        </a>
-                    </div>
-                </td>
+                
+                    <td class="text-center">
+                        @if ($album->name !== 'Uncategorized')
+                            <div>
+                                <a 
+                                    href="{{ route('albums.show', ['album' => $album->id ]) }}" 
+                                    id="ShowIcon" 
+                                    class="__react-root" 
+                                    role="button"
+                                    data-toggle="tooltip"
+                                    title="See info about this album"
+                                    data-placement="top"
+                                    >
+                                </a>
+                            </div>
+                            <div>
+                                <a 
+                                    href="#" 
+                                    id="EditIcon" 
+                                    class="__react-root" 
+                                    role="button"
+                                    data-toggle="tooltip"
+                                    title="Edit this album"
+                                    data-placement="top"
+                                    >
+                                </a>
+                            </div>
+                            <div>
+                                <a 
+                                    href="#" 
+                                    id="DeleteIcon" 
+                                    class="__react-root" 
+                                    role="button"
+                                    data-toggle="tooltip"
+                                    title="Delete this album"
+                                    data-placement="top"
+                                    >
+                                </a>
+                            </div>
+                        @endif
+                    </td>
+
+
             </tr>
         @endforeach
         
