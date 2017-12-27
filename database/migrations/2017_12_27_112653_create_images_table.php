95a102cc48ada6a30837d642c07d8084c1d86703
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-12-27 06:23:42 +0100

    created boilerplates for dedicated image model

---
+++ database/migrations/2017_12_27_112653_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('album_id');
+            $table->integer('carousel_id');
+            $table->string('file_name');
+            $table->string('url_asset');
+            $table->string('url_cache');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
