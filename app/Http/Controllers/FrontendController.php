91809a837d70779ac1e6f205cf008e2ea512ef06
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-01-12 23:05:16 +0700

    restructuring the frontend views

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -4,19 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\PostsController;
+use App\Http\Controllers\CarouselImagesController;
+use App\Page;
+use App\Post;
+Use App\Carousel;
 
 class FrontendController extends Controller
 {
     public function __construct() {
         $this->postInstance = new PostsController();
+        $this->carouselInstance = new CarouselImagesController();
     }
 
     public function home() {
-        return $this->postInstance->index(1);
+        $page = Page::find(1);
+        $posts = $page->posts()->latest()->get();
+        $carousel = $page->carousel;
+        $slides = $carousel->images;
+        
+        return view('frontend.home', compact('posts', 'page', 'slides'));
     }
 
     public function about() {
-        return $this->postInstance->index(2);
+        $page = Page::find(2);
+        $posts = $page->posts()->latest()->get();
+        return view('frontend.about', compact('posts', 'page', 'slides'));
     }
 
     public function gallery() {
