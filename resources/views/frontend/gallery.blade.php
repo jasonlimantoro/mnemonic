6303e387d405747c1ddee0fbbf8353c1a10fa1ff
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-01-26 13:31:53 +0800

    minor styling on the gallery frontend

--- resources/views/frontend/gallery.blade.php
+++ resources/views/frontend/gallery.blade.php
@@ -2,31 +2,67 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-offset-2 col-md-8">
-            @foreach ($albums as $album)
-
-                @component('layouts.thumbnail')
-                    @slot('thumbnailImage')
-                        <img src="{{ $album->featuredImage()->first()['url_cache'] }}" alt="No featured image" class="img-responsive">
+        <div class="col-md-12">
+            @if (!request('album'))
+                @component('layouts.carousel', ['carouselIndicators' => ''])
+                    @slot('carouselId')
+                        galleryCarousel
                     @endslot
-
-                    @slot('thumbnailCaption')
-                        <h1>
-                            <a href="/gallery?album={{ $album->id }}">
-                                {{ $album->name }}
+            
+                    @slot('carouselSlides')
+                        @foreach($albums as $album)
+                            @if ($loop->first)
+                                {{-- This is the first iteration --}}
+                                <div class="item active">
+                            @else
+                                {{--  The remaining iteration  --}}
+                                <div class="item">
+                            @endif
+                                <img src="{{ $album->featuredImage()->first()['url_cache'] }}" alt="album" class="img-responsive">
+                                <div class="carousel-caption">
+                                    <h4>
+                                        <a href="/gallery?album={{ $album->id }}">
+                                            {{ $album->name }}
+                                        </a>
+                                    </h4>
+                                </div>
+                            </div>
+                        @endforeach
+                    @endslot
+            
+                    @slot('carouselControls')
+                        @if(!empty($albums->count()))
+                            <a class="left carousel-control" href="#galleryCarousel" role="button" data-slide="prev">
+                                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
                             </a>
-                        </h1>
+                            <a class="right carousel-control" href="#galleryCarousel" role="button" data-slide="next">
+                                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        @endif
+                        
                     @endslot
                 @endcomponent
-                
-                @if (request('album'))
-                    @foreach ($album->images as $image)
-                        <img src="{{ $image->url_cache }}" alt="" class="img-responsive">
-                    @endforeach
-                @endif
+            @endif
+            
+            @if (request('album'))
+                @foreach ($albums as $album)
+                    @component('layouts.thumbnail')
+                        @slot('thumbnailImage')
+                            <img src="{{ $album->featuredImage()->first()['url_cache'] }}" alt="No featured image" class="img-responsive">
+                        @endslot
 
-                <hr>
-            @endforeach
+                        @slot('thumbnailCaption')
+                            <h1>{{ $album->name }}</h1>
+                        @endslot
+                        @endforeach
+                    @endcomponent
+                @foreach ($album->images as $image)
+                    <img src="{{ $image->url_cache }}" alt="" class="img-responsive">
+                    <hr>
+                @endforeach
+            @endif
         </div>
     </div>
 @endsection
