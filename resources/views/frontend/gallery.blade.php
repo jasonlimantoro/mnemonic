5c1ca94d207362c5149a211ed0f530821d4a603f
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-01-26 09:53:25 +0800

    only featured images are shown without query string to album

--- resources/views/frontend/gallery.blade.php
+++ resources/views/frontend/gallery.blade.php
@@ -4,15 +4,28 @@
     <div class="row">
         <div class="col-md-offset-2 col-md-8">
             @foreach ($albums as $album)
-                <h1>
-                    <a href="/gallery?album={{ $album->id }}">
-                        {{ $album->name }}
-                    </a>
-                </h1>
 
-                @foreach ($album->images as $image)
-                    <img src="{{ $image->url_cache }}" alt="" class="img-responsive">
-                @endforeach
+                @component('layouts.thumbnail')
+                    @slot('thumbnailImage')
+                        <img src="{{ $album->featuredImage()->first()['url_cache'] }}" alt="No featured image" class="img-responsive">
+                    @endslot
+
+                    @slot('thumbnailCaption')
+                        <h1>
+                            <a href="/gallery?album={{ $album->id }}">
+                                {{ $album->name }}
+                            </a>
+                        </h1>
+                    @endslot
+                @endcomponent
+                
+                @if (request('album'))
+                    @foreach ($album->images as $image)
+                        <img src="{{ $image->url_cache }}" alt="" class="img-responsive">
+                    @endforeach
+                @endif
+
+                <hr>
             @endforeach
         </div>
     </div>
