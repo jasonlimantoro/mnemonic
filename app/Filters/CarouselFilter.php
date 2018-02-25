ca2faf19ac17d80de6110f782c912232952e07eb
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-02-26 02:16:55 +0800

    CarouselFilter is a subclass of GalleryFilter

--- app/Filters/CarouselFilter.php
+++ app/Filters/CarouselFilter.php
@@ -5,43 +5,19 @@ namespace App\Filters;
 use Intervention\Image\Facades\Image;
 use Intervention\Image\Filters\FilterInterface;
 
-class CarouselFilter implements FilterInterface
+class CarouselFilter extends GalleryFilter
 {
-    /**
-     * Default size of filter effects
-     */
-    const DEFAULT_CANVAS_WIDTH = 2000;
-    const DEFAULT_CANVAS_HEIGHT = 1000;
-
-
-    /**
-     * Creates new instance of filter
-     *
-     * @param integer $size
-     */
-    public function __construct(
-        $canvasWidth = self::DEFAULT_CANVAS_WIDTH, 
-        $canvasHeight = self::DEFAULT_CANVAS_HEIGHT
-    )
-    {
-        $this->canvasWidth = $canvasWidth;
-        $this->canvasHeight = $canvasHeight;
-        $this->canvas = Image::canvas($canvasWidth, $canvasHeight);
-    }
-
-    /**
-     * Applies filter effects to given image
-     *
-     * @param  \Intervention\Image\Image $image
-     * @return \Intervention\Image\Image
-     */
     public function applyFilter(\Intervention\Image\Image $image)
     {
-
-        $image->resize($this->canvasWidth, $this->canvasHeight, function($constraint){
-            $constraint->aspectRatio();
-            $constraint->upsize();
-        });
+        $image->resize(
+            parent::DEFAULT_CANVAS_WIDTH, 
+            parent::DEFAULT_CANVAS_HEIGHT,
+            function($constraint)
+            {
+                $constraint->aspectRatio();
+                $constraint->upsize();
+            }
+        );
 
         return $this->canvas->insert($image, 'center');
     }
