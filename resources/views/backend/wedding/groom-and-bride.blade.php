0d07b9a12c2200e2584ab1b94adcf3a8d6bd57c7
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-03-02 17:29:46 +0800

    groom and bride form

--- resources/views/backend/wedding/groom-and-bride.blade.php
+++ resources/views/backend/wedding/groom-and-bride.blade.php
@@ -2,14 +2,91 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-12">
-            @component('layouts.panel', ['backButton' => '', 'addButton' => ''])
-                @slot('title')
-                    Groom and Bride page
+        <div class="col-md-12"> @component('layouts.panel', ['backButton' => '', 'addButton' => '']) 
+                @slot('title') Groom and Bride Information
                 @endslot
-
+            
                 @slot('body')
-                    Groom and Bride body
+                <div class="row">
+                    <form> 
+                        <div class="col-md-6 col-xs-12">
+                            Groom
+                            <div class="form-group"> 
+                                <label for="groomFirstName">First Name</label> 
+                                <input type="text" name="groom_first_name" class="form-control" id="groomFirstName">
+                            </div>
+                            <div class="form-group">
+                                <label for="groomFullName">Full Name</label> 
+                                <input type="text" name="groom_full_name" class="form-control" id="groomFirstName">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" name="groom_relation" class="form-control" value="a son of">
+                            </div>
+                            <hr>
+
+                            Father
+                            <div class="form-group"> 
+                                <label for="fatherGroomFirstName">First Name</label> 
+                                <input type="text" name="father_groom_first_name" class="form-control" id="fatherGroomFirstName">
+                            </div>
+                            <div class="form-group">
+                                <label for="fatherGroomFullName">Full Name</label> 
+                                <input type="text" name="father_groom_full_name" class="form-control" id="fatherGroomFirstName">
+                            </div>
+                            <hr>
+
+                            Mother
+                            <div class="form-group"> 
+                                <label for="motherGroomFirstName">First Name</label> 
+                                <input type="text" name="mother_groom_first_name" class="form-control" id="motherGroomFirstName">
+                            </div>
+                            <div class="form-group">
+                                <label for="motherGroomFullName">Full Name</label> 
+                                <input type="text" name="mother_groom_full_name" class="form-control" id="motherGroomFirstName">
+                            </div>
+
+                        </div>
+                        <div class="col-md-6 col-xs-12">
+                            <form> 
+                                Bride
+                                <div class="form-group"> 
+                                    <label for="brideFirstName">First Name</label> 
+                                    <input type="text" name="bride_first_name" class="form-control" id="brideFirstName">
+                                </div>
+                                <div class="form-group">
+                                    <label for="brideFullName">Full Name</label> 
+                                    <input type="text" name="bride_full_name" class="form-control" id="brideFirstName">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" name="bride_relation" class="form-control" value="a daughter of">
+                                </div>
+                                <hr>
+    
+                                Father
+                                <div class="form-group"> 
+                                    <label for="fatherBrideFirstName">First Name</label> 
+                                    <input type="text" name="father_bride_first_name" class="form-control" id="fatherBrideFirstName">
+                                </div>
+                                <div class="form-group">
+                                    <label for="fatherBrideFullName">Full Name</label> 
+                                    <input type="text" name="father_bride_full_name" class="form-control" id="fatherBrideFullName">
+                                </div>
+                                <hr>
+    
+                                Mother
+                                <div class="form-group"> 
+                                    <label for="motherBrideFirstName">First Name</label> 
+                                    <input type="text" name="mother_bride_first_name" class="form-control" id="motherBrideFirstName">
+                                </div>
+                                <div class="form-group">
+                                    <label for="motherBrideFullName">Full Name</label> 
+                                    <input type="text" name="mother_bride_full_name" class="form-control" id="motherBrideFullName">
+                                </div>
+                                
+                            </form>
+                        </div>
+                    </form>
+                </div> 
                 @endslot
             @endcomponent
         </div>
