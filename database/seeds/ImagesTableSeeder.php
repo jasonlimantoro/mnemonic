59afce6883410314a411b17d332bf80fa4c0d3e5
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-03-10 00:06:26 +0700

    added images for couple for seeding

--- database/seeds/ImagesTableSeeder.php
+++ database/seeds/ImagesTableSeeder.php
@@ -20,7 +20,7 @@ class ImagesTableSeeder extends Seeder
                     'featured' => 1,
                     'caption' => null,
                     'file_name' => 'engagement-1.jpg',
-                    'url_asset' => 'https://mnemonic.dev/uploads/album/engagement-1.jpg',
+                    'url_asset' => 'https://mnemonic.dev/uploads/engagement-1.jpg',
                     'url_cache' => 'https://mnemonic.dev/imagecache/gallery/engagement-1.jpg',
                     'created_at' => NOW(),
                     'updated_at' => NOW()
@@ -31,7 +31,7 @@ class ImagesTableSeeder extends Seeder
                     'featured' => 0,
                     'caption' => null,
                     'file_name' => 'engagement-2.jpg',
-                    'url_asset' => 'https://mnemonic.dev/uploads/album/engagement-2.jpg',
+                    'url_asset' => 'https://mnemonic.dev/uploads/engagement-2.jpg',
                     'url_cache' => 'https://mnemonic.dev/imagecache/gallery/engagement-2.jpg',
                     'created_at' => NOW(),
                     'updated_at' => NOW()
@@ -42,7 +42,7 @@ class ImagesTableSeeder extends Seeder
                     'featured' => 1,
                     'caption' => null,
                     'file_name' => 'wisuda-1.png',
-                    'url_asset' => 'https://mnemonic.dev/uploads/album/wisuda-1.png',
+                    'url_asset' => 'https://mnemonic.dev/uploads/wisuda-1.png',
                     'url_cache' => 'https://mnemonic.dev/imagecache/gallery/wisuda-1.png',
                     'created_at' => NOW(),
                     'updated_at' => NOW()
@@ -53,7 +53,7 @@ class ImagesTableSeeder extends Seeder
                     'featured' => 0,
                     'caption' => 'Victor lenny 2',
                     'file_name' => 'victor-lenny-2.jpg',
-                    'url_asset' => 'https://mnemonic.dev/uploads/album/victor-lenny-2.jpg',
+                    'url_asset' => 'https://mnemonic.dev/uploads/victor-lenny-2.jpg',
                     'url_cache' => 'https://mnemonic.dev/imagecache/gallery/victor-lenny-2.jpg',
                     'created_at' => NOW(),
                     'updated_at' => NOW()
@@ -64,7 +64,7 @@ class ImagesTableSeeder extends Seeder
                     'featured' => 0,
                     'caption' => 'Victor Lenny 3',
                     'file_name' => 'victor-lenny-3.jpg',
-                    'url_asset' => 'https://mnemonic.dev/uploads/album/victor-lenny-3.jpg',
+                    'url_asset' => 'https://mnemonic.dev/uploads/victor-lenny-3.jpg',
                     'url_cache' => 'https://mnemonic.dev/imagecache/gallery/victor-lenny-3.jpg',
                     'created_at' => NOW(),
                     'updated_at' => NOW()
@@ -86,8 +86,30 @@ class ImagesTableSeeder extends Seeder
                     'featured' => 1,
                     'caption' => 'Backkk',
                     'file_name' => 'victor-lenny-1.jpg',
-                    'url_asset' => 'https://mnemonic.dev/uploads/album/victor-lenny-1.jpg',
+                    'url_asset' => 'https://mnemonic.dev/uploads/victor-lenny-1.jpg',
                     'url_cache' => 'https://mnemonic.dev/imagecache/gallery/victor-lenny-1.jpg',
+                    'created_at' => NOW(),
+                    'updated_at' => NOW()
+                ],
+                [
+                    'album_id' => 4,
+                    'carousel_id' => null,
+                    'featured' => 0,
+                    'caption' => null,
+                    'file_name' => 'groom.jpg',
+                    'url_asset' => 'https://mnemonic.dev/uploads/groom.jpg',
+                    'url_cache' => 'https://mnemonic.dev/imagecache/gallery/groom.jpg',
+                    'created_at' => NOW(),
+                    'updated_at' => NOW()
+                ],
+                [
+                    'album_id' => 4,
+                    'carousel_id' => null,
+                    'featured' => 0,
+                    'caption' => null,
+                    'file_name' => 'bride.jpg',
+                    'url_asset' => 'https://mnemonic.dev/uploads/bride.jpg',
+                    'url_cache' => 'https://mnemonic.dev/imagecache/gallery/bride.jpg',
                     'created_at' => NOW(),
                     'updated_at' => NOW()
                 ],
