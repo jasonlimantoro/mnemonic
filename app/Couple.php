074f1b6dcae24253bb31a8b3a4c72d7355beefe2
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-03-10 13:22:53 +0700

    no timestamp for couple model

--- app/Couple.php
+++ app/Couple.php
@@ -7,6 +7,9 @@ class Couple extends Model
 {
 	// custom table name
 	protected $table = 'couple';
+
+	// no timestamps is needed
+	public $timestamps = false;
 	
 	public function image(){
 		return $this->hasOne(Image::class);
