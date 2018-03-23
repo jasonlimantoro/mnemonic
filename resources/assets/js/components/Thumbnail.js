16c2f39f7cd7e6f7998fe107c68928d2109895c5
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-03-23 23:38:04 +0800

    refactoring Thumbnail

--- resources/assets/js/components/Thumbnail.js
+++ resources/assets/js/components/Thumbnail.js
@@ -1,29 +1,26 @@
 import React from "react";
 import {Grid, Row, Col, Thumbnail, Button} from "react-bootstrap";
-import { DisplayImagesFromSelectedGallery } from "./DisplayImage";
+import { DisplayImagesFromSelectedGallery} from "./DisplayImage";
 
 export class ThumbnailGallery extends React.Component {
     constructor(props){
         super(props);
     }
 
-
     render() {
-        const activeClass = this.props.isActive ? 'active' : '';
-        var display = '';
-        if(this.props.isActive) {
-            display = <DisplayImagesFromSelectedGallery file={this.props.selectedImage} i={this.props.i} />;
-        }
-        
+		const isActive = this.props.isActive;
         return (
             <Thumbnail 
                 src={this.props.sourceImage} 
-                className={'thumbnail-gallery ' + activeClass}
+                className={isActive ? 'thumbnail-gallery active' : 'thumbnail-gallery'}
                 alt="242x200"
             >
                 <strong>{this.props.title}</strong>
                 <p>{this.props.description}</p>
-                {display}
+				{isActive ? 
+					<DisplayImagesFromSelectedGallery file={this.props.selectedImage} i={this.props.i} fromGallery />
+				:	null 
+				}
                 
             </Thumbnail>
         )
