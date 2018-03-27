1c0b6aed0cb0dee96ac044b9e2490c48098ad34b
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-03-28 02:13:13 +0800

    remove unused javascript files and components

--- resources/assets/js/components/Form.js
+++ resources/assets/js/components/Form.js
@@ -11,34 +11,6 @@ function FieldGroup ( {id, label, labelClass, ...props}) {
     )
 }
 
-export const FormPost = (props) => {
-    return (
-        <form method="POST" action={props.action}>
-            <FieldGroup
-                id = "formControlsTitle"
-                type = "text"
-                label = "Title"
-                placeholder = "Enter title"
-                name = "title"
-                value = {props.titleValue}
-            />
-
-            <FormGroup controlId="formControlsTextarea">
-                <ControlLabel>Body</ControlLabel>
-                <FormControl 
-                    componentClass="textarea" 
-                    placeholder="Enter Body" 
-                    name="body"
-                    cols="30"
-                    rows="10"
-                /> {props.bodyValue}
-            </FormGroup>
-
-            <PrimaryButton type="submit" text="Publish" />
-        </form>
-    )
-}
-
 export class SearchBox extends React.Component {
     constructor(props) {
         super(props);
