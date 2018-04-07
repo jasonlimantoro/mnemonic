0515bfc1213cb3391eb04a8531d670c8861b3f90
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-04-08 01:40:50 +0800

    adding more methods to repositories

--- app/Repositories/Albums.php
+++ app/Repositories/Albums.php
@@ -15,4 +15,14 @@ class Albums
 	{
 		return Album::with('images')->oldest()->get();	
 	}
+	
+	public function categorized()
+	{
+		return Album::where('name', '!=', 'Uncategorized')->oldest()->get();
+	}
+
+	public function uncategorized()
+	{
+		return Album::where('name', 'Uncategorized')->first();
+	}
 }
