b0d87515e67b0cb5039ad686e670d6c931e8c58e
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-04-08 01:57:26 +0800

    remove unused methods

--- app/Couple.php
+++ app/Couple.php
@@ -14,29 +14,7 @@ class Couple extends Model
 	// eager load
 	protected $with = ['image'];
 
-	public $defaultState;
-
-	public function __construct(array $attributes = [])
-	{
-		parent::__construct($attributes);
-		$this->defaultState = Album::uncategorizedAlbum();
-	}
-
 	public function image(){
 		return $this->morphOne(Image::class, 'imageable');
-	}
-
-	public function addImage($coupleImage){
-		if($oldImage = $this->image()){
-			// delete the old image
-			$oldImage->delete();
-		}
-		$imageExist = Image::firstorNew($coupleImage);
-		if(!$imageExist->exists){
-			// add to uncategorized album if this is a new resource
-			$this->defaultState->images()->create($coupleImage);
-		}
-		// create a new Image for this couple
-		$this->image()->create($coupleImage);
 	}
 }
