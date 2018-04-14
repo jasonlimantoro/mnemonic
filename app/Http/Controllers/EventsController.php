7507735fde68f2cd7d5055a4a4e1c7598443273c
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-04-15 01:40:56 +0800

    incorrect argument for create in store method

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -8,95 +8,93 @@ use App\Image;
 
 class EventsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
 		$events = Event::all();
-		return view('backend.wedding.events.index', compact('events')); 
-    }
+		return view('backend.wedding.events.index', compact('events'));
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('backend.wedding.events.create');
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		return view('backend.wedding.events.create');
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-       $rules = [
-		   'name' => 'required',
-		   'datetime' => 'required|date|after:today'
-	   ];
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		$rules = [
+			'name' => 'required',
+			'datetime' => 'required|date|after:today'
+		];
 
-	   $this->validate($request, $rules);
-	   $event = Event::create([
-		   request(['name', 'description', 'location', 'datetime'])
-	   ]);
-	   if($eventImage = Image::handleUpload($request))
-	   {
+		$this->validate($request, $rules);
+		$event = Event::create(
+			request(['name', 'description', 'location', 'datetime'])
+		);
+		if ($eventImage = Image::handleUpload($request)) {
 			$eventImage->addTo($event);
-	   }
+		}
 
-	   \Session::flash('success_msg', 'Event is successfully created!');
+		\Session::flash('success_msg', 'Event is successfully created!');
 
-	   return back();
+		return back();
 
-    }
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Event  $event
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Event $event)
-    {
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  \App\Event  $event
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Event $event)
+	{
 		return view('backend.wedding.events.show', compact('event'));
-    }
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Event  $event
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Event $event)
-    {
-		$eventImage = $event->image ? $event->image->url_cache : NULL;
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  \App\Event  $event
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit(Event $event)
+	{
+		$eventImage = $event->image ? $event->image->url_cache : null;
 		return view('backend.wedding.events.edit', compact('event', 'eventImage'));
-    }
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Event  $event
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Event $event)
-    {
-        $rules = [
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \App\Event  $event
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, Event $event)
+	{
+		$rules = [
 			'name' => 'required',
 			'datetime' => 'required|date|after:today'
 		];
 		$this->validate($request, $rules);
-		
-		if ($eventImage = Image::handleUpload($request))
-		{
+
+		if ($eventImage = Image::handleUpload($request)) {
 			$eventImage->addTo($event);
 		}
 		$event->update(
@@ -107,22 +105,21 @@ class EventsController extends Controller
 
 		return back();
 
-    }
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Event  $event
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Event $event)
-    {
-		if ($event->image)
-		{
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  \App\Event  $event
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(Event $event)
+	{
+		if ($event->image) {
 			$event->image->delete();
 		}
 		$event->delete();
 		\Session::flash('success_msg', 'Event is successfully deleted!');
 		return back();
-    }
+	}
 }
