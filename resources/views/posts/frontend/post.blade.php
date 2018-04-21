649f9dc690700e2badb9082c1ff3351f81a60241
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-04-21 11:28:11 +0800

    limit up to 300 characters

--- resources/views/posts/frontend/post.blade.php
+++ resources/views/posts/frontend/post.blade.php
@@ -1,12 +1,9 @@
 <div class="blog-post">
-    <div class="col-md-6">
-        <h2>{{ $post->title }}</h2>
-        <p> {{ str_limit($post->body, 200, '...') }} </p>
-        
-        <a href="{{ route('post.read', ['page' => str_slug($page->title), 'post_title' => str_slug($post->title) ]) }}">
-            Read More
-        </a>
-        <hr>
-    </div>
+	<h2>{{ $post->title }}</h2>
+	<p> {{ str_limit($post->body, 300, '...') }} </p>
+	<a href="{{ route('posts.read', ['post' => $post->id ]) }}">
+		Read More
+	</a>
+	<hr>
 </div>
 
