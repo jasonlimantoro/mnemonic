1874c09ad95396616d26aa5125a60721b4244762
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-04-24 01:24:12 +0800

    datetime column is now a Carbon instance, define its accessor

--- app/Event.php
+++ app/Event.php
@@ -4,13 +4,19 @@ namespace App;
 
 use App\Image;
 use App\Album;
+use Carbon\Carbon;
 
 class Event extends Model
 {
+	protected $dates = ['datetime'];
 	
 	public function image()
 	{
 		return $this->morphOne(Image::class, 'imageable');
+	}
 
+	public function getDatetimeAttribute($date)
+	{
+		return Carbon::parse($date)->toDayDateTimeString();
 	}
 }
