e234e48885a5225a2c2c6873d58eadf8e45f301f
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-04-24 01:52:34 +0800

    1 year not 100 years

--- database/factories/EventsFactory.php
+++ database/factories/EventsFactory.php
@@ -7,6 +7,6 @@ $factory->define(App\Event::class, function (Faker $faker) {
 		'name' => $faker->words($nb = 3, $asText = true),
 		'description' => $faker->sentence(),
 		'location' => $faker->address(),
-		'datetime' => $faker->dateTimeBetween($startDate = 'now', $endDate = '+100 years', $timezone = 'Asia/Singapore')
+		'datetime' => $faker->dateTimeBetween($startDate = 'now', $endDate = '+1 years', $timezone = 'Asia/Singapore')
 	];
 });
