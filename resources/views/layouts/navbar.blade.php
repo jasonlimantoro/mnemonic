22327d03c9346697954d013b88f9c7cdece8d245
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-04-23 22:22:19 +0800

    changing route names

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -11,7 +11,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="{{ route('home') }}">
+					<a class="navbar-brand" href="{{ route('front.index') }}">
 						<img src="/images/logo.png" alt="logo" class="img-responsive">
 					</a>
 				</div>
@@ -25,16 +25,16 @@
 			<div class="col-xs-12">
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 					<ul class="nav navbar-nav">
-						<li><a href="/">Home</a></li>
-						<li><a href="/about-us">About Us</a></li>
-						<li><a href="/gallery">Gallery</a></li>
+						<li><a href="{{ route('front.index')}}">Home</a></li>
+						<li><a href="{{ route('front.about')}}">About Us</a></li>
+						<li><a href="{{ route('front.gallery')}}">Gallery</a></li>
 						<li class="hidden-xs">
 							<a href="/" class="navbar-logo-center">
 								<img src="/images/logo.png" alt="logo" class="img-responsive">
 							</a>
 						</li>
-						<li><a href="/wedding-day">Wedding Day</a></li>
-						<li><a href="/online-rsvp">Online RSVP</a></li>
+						<li><a href="{{ route('front.wedding')}}">Wedding Day</a></li>
+						<li><a href="{{ route('front.rsvp')}}">Online RSVP</a></li>
 						<li class="visible-xs"><a href="{{ route('admin') }}">Backend</a></li>
 					</ul>
 
