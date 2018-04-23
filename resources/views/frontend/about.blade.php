baa22277a9ab060830bc3f1577f70ca1072461eb
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-04-24 03:23:34 +0800

    minor fix for front.about

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -2,19 +2,19 @@
 
 
 @section('content')
-	<div class="container about-us-main">
+	<div class="container about-us-container">
 		<div class="col-md-12">
-			<div class="row page-title">
-				<h1>About Us</h1>
+			<div class="row page-title font-theme">
+				<h1 class="color-theme">About Us</h1>
 			</div>
-			<div class="row about-us-container">
+			<div class="row">
 				@foreach($couple as $c)
 					<div class="col-md-12 about-us-content">
 						<div class="col-md-4 couple-image">
 							<img src="{{ $c->image->url_cache }}" alt="{{ $c->name }}" class="img-responsive"> 
 						</div>
 						<div class="col-md-8 couple-description">
-							<div class="col-md-12 couple-name">
+							<div class="col-md-12 couple-name font-theme">
 								<h1> {{ $c->name }} </h1>
 							</div>
 							
