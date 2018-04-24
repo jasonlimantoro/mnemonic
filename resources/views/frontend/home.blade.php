ffd7d61aa58d15971499c0b5121ad9c53d1bf803
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-04-24 18:40:30 +0800

    clean unused slots

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -26,19 +26,6 @@
         </div>
       @endforeach
     @endslot
-
-    {{-- @slot('carouselControls')
-      @if(!empty($slides->count()))
-        <a class="left carousel-control" href="#mainCarousel" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#mainCarousel" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      @endif
-    @endslot --}}
   @endcomponent
 
   <div class="row">
