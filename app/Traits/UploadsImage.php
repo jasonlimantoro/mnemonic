64dc5d81603e4f3444301fe554f3f80782520784
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-04-24 13:52:33 +0800

    beautify traits

--- app/Traits/UploadsImage.php
+++ app/Traits/UploadsImage.php
@@ -9,76 +9,94 @@ use App\Filters\GalleryFilter;
 
 trait UploadsImage {
 
+	/** 
+	 * Filter the uploaded file and save it to the filesystem
+	 * 
+	 * @param  Illuminate\Http\Request $request
+	 * @return mixed
+	 * 
+	*/
 	public static function handleUpload($request)
 	{
         $newImage = $request->file('image');
-        $galleryImageName = $request->gallery_image;
+		$existingImageName = $request->gallery_image;
+
+		// new image
         if($newImage){
-            // new image
             $newImageName = $newImage->getClientOriginalName();
             $uploadPath = public_path('uploads/' . $newImageName);
             $imgFiltered = \Image::make($newImage);
-            $galleryImageName = $newImageName;
+            $resultImageName = $newImageName;
         }
 
-        else if ($galleryImageName) {
-            // existing images
-            $galleryPath = public_path('uploads/' . $galleryImageName);
-            $imgFiltered = \Image::make($galleryPath);
-            $uploadPath = $galleryPath;
-        }
+		// existing images
+        else if ($existingImageName) {
+            $uploadPath = public_path('uploads/' . $existingImageName);
+			$imgFiltered = \Image::make($uploadPath);
+			$resultImageName = $existingImageName;
+		}
+
+		// no new or gallery images in the request
         else {
-            // no new or gallery images in the request
             return null;
         }
 
-        // applyFilter GalleryFilter and save it to file system
+        // ApplyFilter GalleryFilter and save it to file system
         $imgFiltered->filter(new GalleryFilter)->save($uploadPath);
 
         // array
         $imageAttr = [
-            'file_name' => $galleryImageName,
-            'url_asset' => url('uploads/' . $galleryImageName),
-            'url_cache' => url('/imagecache/gallery/' . $galleryImageName)
+            'file_name' => $resultImageName,
+            'url_asset' => url('uploads/' . $resultImageName),
+            'url_cache' => url('/imagecache/gallery/' . $resultImageName)
         ];
 
         return Image::firstOrNew($imageAttr);
         
-    }
+	}
+
+    /**
+     * Add image to the owner class.
+     *
+     * @param  array  $attributes
+	 * @param  model  $updatable
+     * @return mixed
+	 * 
+     */
 
 	public function addTo($ownerClass, $updatable = NULL)
 	{
 
+		// relevant attributes
 		$imgAttr = $this->only('file_name', 'url_asset', 'url_cache');
 
-		// the owner has an exisiting polymorphic one-to-one relationship
+		// the owner has an 'exisiting' polymorphic one-to-one relationship
 		if($ownerClass->image)
 		{
 			$ownerClass->image()->delete();
 		}
 
-		// a new image
-		if(!$this->exists)
+		// a new image and not currently being added to any album 
+		if(!$this->exists && !$ownerClass instanceof Album)
 		{
-			// if it's not added to album
-			if (!$ownerClass instanceof Album) 
-			{
-				$this->assignToUncategorizedAlbum($this->attributes);
-			}
+			$this->assignToUncategorizedAlbum($this->attributes);
 		}
 			
+		// Update the image only, no need to create new relationships
 		if($updatable)
 		{
-			// update the image only
 			$updatable->update($imgAttr);
 			return 1;
 		}
 
+		// Create the relationships
+		// morphMany relationships
 		if($ownerClass->images)
 		{
 			return $ownerClass->images()->create($imgAttr);
 		}
 
+		// morphOne relationships
 		else {
 			return $ownerClass->image()->create($imgAttr);
 		}
@@ -87,6 +105,13 @@ trait UploadsImage {
 
 	}
 
+	/**
+	 * Assign image with given attributes to Uncategorized Album
+	 * 
+	 * @param  array $attributes
+	 * @return void
+	 * 
+	 */
 	public function assignToUncategorizedAlbum(array $attributes)
 	{
 		(new Albums)->uncategorized()
