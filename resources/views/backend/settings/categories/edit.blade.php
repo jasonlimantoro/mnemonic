6ac420ccf83d24c4b5b956bdf7449cfb5a12d650
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-04-28 01:37:42 +0800

    extracting categories' form

--- resources/views/backend/settings/categories/edit.blade.php
+++ resources/views/backend/settings/categories/edit.blade.php
@@ -1,49 +1,25 @@
 @extends('layouts.submaster')
 
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
-            @component('layouts.panel', [
-				'title' => "Edit a category"
-			])
-                @slot('backButton')
-                    @component('layouts.backButton', [
-                        'text' => 'Show all categories',
-                        'url' => route('categories.index')
-                    ])
-                        
-                    @endcomponent
-                @endslot
+  <div class="row">
+    <div class="col-md-12">
+      @component('layouts.panel', [
+        'title' => "Edit a category"
+      ])
+        @slot('backButton')
+          @component('layouts.backButton', [
+            'text' => 'Show all categories',
+            'url' => route('categories.index')
+          ])
+            
+          @endcomponent
+        @endslot
 				@slot('body')
-					<form action="{{ route('categories.update', ['category' => $category->id ]) }}" method="POST">
-						{{ method_field('PATCH') }}
-						<div class="form-group">
-							<label for="formControlName">Category Name</label>
-							<input 
-								type="text"
-								class="form-control"
-								id="formControlName"
-								name="name"
-								placeholder="Enter name"
-								value="{{ $category->name }}"
-							>
-						</div>
-
-						<div class="form-group">
-							<label for="formControlDescription">Category Description</label>
-							<textarea 
-								name="description"
-								id="formControlDescription" 
-								cols="30" 
-								rows="10" 
-								placeholder="Enter Description" 
-								class="form-control"
-							>{{ $category->description}}</textarea>
-						</div>
-						<button type="submit" class="btn btn-primary">Publish</button>
-					</form>
-                @endslot
-            @endcomponent
-        </div>
+					{{ Form::model($category, ['route' => ['categories.update', $category->id], 'method' => 'PATCH']) }}
+						@include('backend.settings.categories.form', ['submitButtonText' => 'Update Category'])
+					{{ Form::close() }}
+        @endslot
+      @endcomponent
     </div>
+  </div>
 @endsection
---
+++ resources/views/backend/settings/categories/form.blade.php
@@ -0,0 +1,16 @@
+{{-- name field --}}
+<div class="form-group">
+	{{ Form::label('name', 'Name:') }}
+	{{ Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Enter Name']) }}
+</div>
+
+{{-- description field --}}
+<div class="form-group">
+	{{ Form::label('description', 'Description:') }}
+	{{ Form::textarea('description', null, ['class' => 'form-control', 'placeholder' => 'Enter Description']) }}
+</div>
+
+{{-- Submit Button --}}
+<div class="form-group">
+	{{ Form::submit($submitButtonText, ['class' => 'btn btn-primary']) }}
+</div>
