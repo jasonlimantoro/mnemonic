d8b88d665a0cbb9289327d50b029fad1ef5d6cc6
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-04-28 22:43:21 +0800

    assignToCategorizedAlbum belongs to the Image model

--- app/Image.php
+++ app/Image.php
@@ -3,11 +3,20 @@
 namespace App;
 
 use App\Traits\UploadsImage;
+use App\Repositories\Albums;
 
 
 class Image extends Model
 {
 	use UploadsImage;
+
+	protected $albums;
+	
+	public function __construct(array $attributes = [])
+	{
+		parent::__construct($attributes);
+		$this->albums = new Albums;
+	}
 
 	public function imageable(){
 		return $this->morphTo();
@@ -15,5 +24,19 @@ class Image extends Model
 
     public function isFeatured(){
         return $this->featured;
+	}
+
+	/**
+	 * Assign image with given attributes to Uncategorized Album
+	 * 
+	 * @param  array $attributes
+	 * @return void
+	 * 
+	 */
+	public function assignToUncategorizedAlbum(array $attributes)
+	{
+		$this->albums->uncategorized()
+			   		->images()
+			   		->create($attributes);
 	}
 }
