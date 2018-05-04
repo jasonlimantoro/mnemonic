23c46ed60a3ee6eed773ee8e642bfb8d685ee35b
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-05 03:30:44 +0800

    generating php docblocks

--- app/Setting.php
+++ app/Setting.php
@@ -5,29 +5,65 @@ namespace App;
 
 class Setting extends Model
 {
-   public $timestamps = false;
+    /**
+     * Timestamp is not needed
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 
-   public static function byKey($key)
+    /**
+     *  Retrieve the record given the key
+     *
+     * @param string $key
+     * @return Setting|\Illuminate\Database\Eloquent\Model|null
+     */
+    public static function byKey($key)
    {
 	   return static::where('key', $key)->first();
    }
 
-   public function getValue()
+    /**
+     * Retrieve the record's value
+     *
+     * @return mixed
+     */
+    public function getValue()
    {
 	   return $this->value;
    }
 
-   public static function getValueByKey($key)
+    /**
+     * Retrieve the value of the record given the key
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public static function getValueByKey($key)
    {
 	   return static::byKey($key)->getValue();
    }
 
-   public function updateValue($value)
+
+    /**
+     *  Update the value field
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    public function updateValue($value)
    {
 	   return $this->update(['value' => $value]);
    }
 
-   public static function updateValueByKey($key, $value)
+    /**
+     *  Update the record, given key, with the value
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return bool
+     */
+    public static function updateValueByKey(string $key, $value)
    {
 	   return static::byKey($key)->update(['value' => $value]);
    }
