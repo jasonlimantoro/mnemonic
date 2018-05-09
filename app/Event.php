596eb7e5226286dcb7609329d1ac6d511b8f1301
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-10 01:49:32 +0800

    fix setter issues

--- app/Event.php
+++ app/Event.php
@@ -8,33 +8,35 @@ use Collective\Html\Eloquent\FormAccessible;
 
 class Event extends Model
 {
-	use FormAccessible, FiltersSearch;
-	protected $dates = ['datetime'];
-	
-	public function image()
-	{
-		return $this->morphOne(Image::class, 'imageable');
-	}
+    use FormAccessible, FiltersSearch;
+    protected $dates = ['datetime'];
 
-	public function getDatetimeAttribute($date)
-	{
-		return Carbon::parse($date)->format('l - M jS, Y'); // Friday - October 20th, 2017
-	}
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
 
-	/**
+    public function getDatetimeAttribute($date)
+    {
+        return Carbon::parse($date)->format('l - M jS, Y'); // Friday - October 20th, 2017
+    }
+
+    /**
      * Get the event's datetime for forms.
      *
      * @param  string  $value
      * @return string
      */
-	
-	public function formDatetimeAttribute($value)
-	{
-		return Carbon::parse($value)->format('Y-m-d\TH:i');
-	}
+    public function formDatetimeAttribute($value)
+    {
+        return Carbon::parse($value)->format('Y-m-d\TH:i');
+    }
 
-	public function setDatetimeAttribute($date)
-	{
-		$this->attributes['datetime'] = Carbon::parse($date);
-	}
+    public function setDatetimeAttribute($date)
+    {
+        if (!($date instanceof \DateTime)) {
+            $date = Carbon::parse($date);
+        }
+        $this->attributes['datetime'] = $date;
+    }
 }
