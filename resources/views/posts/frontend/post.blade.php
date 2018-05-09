d693d969bc884e395cbf82b2610d07d1a13a2bd0
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-09 15:59:49 +0800

    fix body into description

--- resources/views/posts/frontend/post.blade.php
+++ resources/views/posts/frontend/post.blade.php
@@ -1,6 +1,6 @@
 <div class="blog-post">
 	<h2>{{ $post->title }}</h2>
-	<p> {{ str_limit($post->body, 300, '...') }} </p>
+	<p> {{ str_limit($post->description, 300, '...') }} </p>
 	<a href="{{ route('posts.read', ['post' => $post->id ]) }}">
 		Read More
 	</a>
