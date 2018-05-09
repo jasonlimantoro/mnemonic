697db77628c518de99b6bc292802b72d9672f0f6
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-09 18:47:40 +0800

    fix image factory

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -1,17 +1,15 @@
 <?php
 
 use Faker\Generator as Faker;
-use PHPUnit\Framework\Constraint\IsFalse;
+use App\Album;
 
 $factory->define(App\Image::class, function (Faker $faker) {
     return [
-        'album_id' => function(){
-            return factory(App\Album::class)->create()->id;
-        },
-        // 'album_id' => $faker->numberBetween($min = 1, $max = 4),
         'caption' => $faker->sentence,
         'file_name' => $faker->word . '.jpg',
         'url_asset' => $faker->imageUrl($width = 640, $height = 480),
-        'url_cache' => $faker->imageUrl($width = 640, $height = 480, 'cats')
+		'url_cache' => $faker->imageUrl($width = 640, $height = 480, 'cats'),
+		'imageable_id' => 4,  
+		'imageable_type' => 'App\\Album',
     ];
 });
