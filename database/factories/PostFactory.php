3eaff0dd54ee77f682b3e05cd072805a87ba994e
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-09 22:37:06 +0800

    fix post's field name

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -7,7 +7,7 @@ $factory->define(App\Post::class, function (Faker $faker) {
         'user_id' => $faker->numberBetween($min = 1, $max = 2),
         'page_id' => $faker->numberBetween($min = 1, $max = 2),
         'title' => str_replace('.', '', title_case($faker->sentence())),
-        'body'=> $faker->paragraph($nbSentences = 30, $variableNbSentences = true),
+        'description'=> $faker->paragraph($nbSentences = 30, $variableNbSentences = true),
         'created_at' => $faker->dateTimeBetween($startDate = '-1 years', $endDate = 'now', $timezone = 'Asia/Singapore'),
         'updated_at' => $faker->dateTimeBetween($startDate = '-1 years', $endDate = 'now', $timezone = 'Asia/Singapore')
     ];
--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,24 +12,23 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-
         DB::table('posts')->insert(
             [
                 [
-					'user_id' => 1,
-					'page_id' => 2, 
-					'title' => "Victor Immanuel Rumende, S. Kom",
-					'body' => "Surabaya, 21 Juni 1990",
-					'created_at' => NOW(),
-					'updated_at' => NOW()
+                    'user_id' => 1,
+                    'page_id' => 2,
+                    'title' => 'Victor Immanuel Rumende, S. Kom',
+                    'description' => 'Surabaya, 21 Juni 1990',
+                    'created_at' => NOW(),
+                    'updated_at' => NOW()
                 ],
                 [
-					'user_id' => 2,
-					'page_id' => 2, 
-					'title' => "Lenny Kurniawati Ligadjaja, S. E",
-					'body' => "Surabaya, 03 Maret 1992",
-					'created_at' => NOW(),
-					'updated_at' => NOW()
+                    'user_id' => 2,
+                    'page_id' => 2,
+                    'title' => 'Lenny Kurniawati Ligadjaja, S. E',
+                    'description' => 'Surabaya, 03 Maret 1992',
+                    'created_at' => NOW(),
+                    'updated_at' => NOW()
                 ]
             ]
         );
