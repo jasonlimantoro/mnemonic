d2cfd4645503d4e82e270e5d1c7e54de24db7389
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-10 22:35:43 +0800

    refactoring, and saving to file system using laravel's put method

--- app/Traits/UploadsImage.php
+++ app/Traits/UploadsImage.php
@@ -5,44 +5,47 @@ namespace App\Traits;
 use App\Album;
 use App\Image;
 use App\Filters\GalleryFilter;
+use Illuminate\Support\Facades\Storage;
 
-trait UploadsImage {
+trait UploadsImage
+{
+    abstract public function assignToUncategorizedAlbum();
 
-	abstract public function assignToUncategorizedAlbum();
-	/** 
-	 * Filter the uploaded file and save it to the filesystem
-	 * 
-	 * @param  \Illuminate\Http\Request $request
-	 * @return mixed
-	 * 
-	*/
-	public static function handleUpload($request)
-	{
+    /**
+     * Filter the uploaded file and save it to the filesystem
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return mixed
+     *
+    */
+    public static function handleUpload($request)
+    {
         $newImage = $request->file('image');
-		$existingImageName = $request->gallery_image;
+        $existingImageName = $request->gallery_image;
 
-		// new image
-        if($newImage){
+        // new image
+        if ($newImage) {
             $newImageName = $newImage->getClientOriginalName();
             $uploadPath = public_path('uploads/' . $newImageName);
             $imgFiltered = \Image::make($newImage);
             $resultImageName = $newImageName;
         }
 
-		// existing images
-        else if ($existingImageName) {
+        // existing images
+        elseif ($existingImageName) {
             $uploadPath = public_path('uploads/' . $existingImageName);
-			$imgFiltered = \Image::make($uploadPath);
-			$resultImageName = $existingImageName;
-		}
+            $imgFiltered = \Image::make($uploadPath);
+            $resultImageName = $existingImageName;
+        }
 
-		// no new or gallery images in the request
+        // no new or gallery images in the request
         else {
             return null;
         }
 
         // ApplyFilter GalleryFilter and save it to file system
-        $imgFiltered->filter(new GalleryFilter)->save($uploadPath);
+        $imgFiltered->filter(new GalleryFilter);
+        $file = Storage::disk('uploads')->put($resultImageName, (string) $imgFiltered->encode());
 
         // array
         $imageAttr = [
@@ -52,50 +55,41 @@ trait UploadsImage {
         ];
 
         return Image::firstOrNew($imageAttr);
-        
-	}
-
+    }
 
     /**
      * @param Model $ownerClass
      * @param Model|null $updatable
      * @return mixed
      */
-    public function addTo($ownerClass, $updatable = NULL)
-	{
+    public function addTo($ownerClass, $updatable = null)
+    {
+        // relevant attributes
+        $imgAttr = $this->only('file_name', 'url_asset', 'url_cache');
 
-		// relevant attributes
-		$imgAttr = $this->only('file_name', 'url_asset', 'url_cache');
+        // the owner has an 'existing' polymorphic one-to-one relationship
+        if ($ownerClass->image) {
+            $ownerClass->image()->delete();
+        }
 
-		// the owner has an 'existing' polymorphic one-to-one relationship
-		if($ownerClass->image)
-		{
-			$ownerClass->image()->delete();
-		}
+        // a new image and not currently being added to any album
+        if (!$this->exists && !$ownerClass instanceof Album) {
+            $this->assignToUncategorizedAlbum($this->attributes);
+        }
 
-		// a new image and not currently being added to any album 
-		if(!$this->exists && !$ownerClass instanceof Album)
-		{
-			$this->assignToUncategorizedAlbum($this->attributes);
-		}
-			
-		// Update the image only, no need to create new relationships
-		if($updatable)
-		{
-			$updatable->update($imgAttr);
-			return 1;
-		}
+        // Update the image only, no need to create new relationships
+        if ($updatable) {
+            $updatable->update($imgAttr);
+        }
 
-		// Create the relationships
-		// morphMany relationships
-		if($ownerClass->images)
-		{
-			return $ownerClass->images()->create($imgAttr);
-		}
+        // Create the relationships
+        // morphMany relationships
+        if ($ownerClass->images) {
+            $ownerClass->images()->create($imgAttr);
+        } else {
+            $ownerClass->image()->create($imgAttr);
+        }
 
-		// morphOne relationships
-		else {
-			return $ownerClass->image()->create($imgAttr);
-		}
-	}
+        return $this;
+    }
 }
