58bd093bdde77a150ec22750ad8e9da58f09199c
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-12 00:22:42 +0800

    fill the value as well for easier testing

--- database/seeds/SettingsTableSeeder.php
+++ database/seeds/SettingsTableSeeder.php
@@ -11,35 +11,52 @@ class SettingsTableSeeder extends Seeder
      */
     public function run()
     {
+		$contact = json_encode([
+			'email' => 'Janedoe@example.com',
+			'phone' => '298-4298-278',
+			'mobile' => '9487-39847-38',
+			'address' => 'Oklahoma Street Avenue no 26',
+			'region' => 'North Carolina',
+			'city' => 'Chicago',
+			'country' => 'USA',
+			'zip_code' => '294982',
+		]);
         DB::table('settings')->insert(
             [
                 [
                     'name' => 'Admin Email',
-                    'key' => 'admin-email'
+					'key' => 'admin-email',
+					'value' => 'myemail@example.com',
                 ],
                 [
                     'name' => 'Site Title',
-                    'key' => 'site-title'
+					'key' => 'site-title',
+					'value' => 'Laracast',
                 ],
                 [
                     'name' => 'Site Description',
-                    'key' => 'site-description'
+                    'key' => 'site-description',
+					'value' => 'Laracast',
                 ],
                 [
                     'name' => 'Contact',
-                    'key' => 'site-contact'
+					'key' => 'site-contact',
+					'value' => $contact
                 ],
                 [
                     'name' => 'Site Logo',
-                    'key' => 'site-logo'
+					'key' => 'site-logo',
+					'value' => null
                 ],
                 [
                     'name' => 'Site Keywords',
-                    'key' => 'site-keywords'
+					'key' => 'site-keywords',
+					'value' => null
                 ],
                 [
                     'name' => 'Site Favicon',
-                    'key' => 'site-favicon'
+					'key' => 'site-favicon',
+					'value' => null
                 ],
             ]
         );
