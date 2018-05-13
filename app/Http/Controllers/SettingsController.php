cc324b101f2c8f63a4c02b0108ce6ef0ae656a5b
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-13 22:43:50 +0800

    refactor update action

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -74,48 +74,8 @@ class SettingsController extends Controller
      */
     public function update(Request $request)
     {
-        Setting::updateManyByKeys([
-            'admin-email' => $request->admin_email,
-            'site-title' => $request->site_title,
-            'site-description' => $request->site_description,
-            'site-contact' => json_encode([
-                'email' => $request->contact_email,
-                'phone' => $request->contact_phone,
-                'mobile' => $request->contact_mobile,
-                'address' => $request->contact_address,
-                'region' => $request->contact_region,
-                'city' => $request->contact_city,
-                'country' => $request->contact_country,
-                'zip_code' => $request->contact_zip_code,
-            ])
-        ]);
 
-
-        if ($favicon = $request->favicon_from_gallery) {
-			Setting::updateValueByKey('site-favicon', Image::byName($favicon)->url_cache);
-		} else if ($favicon = $request->file('favicon_from_local')) {
-			$faviconName = $favicon->getClientOriginalName();
-			$path = Storage::disk('uploads')->putFileAs('/', $favicon, $favicon->getClientOriginalName());
-			$image = Image::create([
-				'file_name' => $faviconName,
-				'url_asset' => url('uploads/' . $faviconName),
-				'url_cache' => url('imagecache/gallery/' . $faviconName)
-			]);
-			Setting::updateValueByKey('site-favicon', $image->url_cache);
-		}
-
-        if ($logo = $request->logo_from_gallery) {
-			Setting::updateValueByKey('site-logo', Image::byName($logo)->url_cache);
-		} else if ($logo = $request->file('logo_from_local')) {
-			$logoName = $logo->getClientOriginalName();
-			$path = Storage::disk('uploads')->putFileAs('/', $logo, $logo->getClientOriginalName());
-			$image = Image::create([
-				'file_name' => $logoName,
-				'url_asset' => url('uploads/' . $logoName),
-				'url_cache' => url('imagecache/gallery/' . $logoName)
-			]);
-			Setting::updateValueByKey('site-logo', $image->url_cache);
-		}
+		Setting::onUpdate($request);
 
         $this->flash('Settings are updated successfully');
 
--- app/Setting.php
+++ app/Setting.php
@@ -2,6 +2,9 @@
 
 namespace App;
 
+use Illuminate\Http\Request;
+
+
 
 class Setting extends Model
 {
@@ -109,5 +112,55 @@ class Setting extends Model
 			static::updateValueByKey($key, $value);
 		}
 	}
-	  
+
+	public static function onUpdate(Request $request)
+	{
+        static::updateManyByKeys([
+            'admin-email' => $request->admin_email,
+            'site-title' => $request->site_title,
+            'site-description' => $request->site_description,
+            'site-contact' => json_encode([
+                'email' => $request->contact_email,
+                'phone' => $request->contact_phone,
+                'mobile' => $request->contact_mobile,
+                'address' => $request->contact_address,
+                'region' => $request->contact_region,
+                'city' => $request->contact_city,
+                'country' => $request->contact_country,
+                'zip_code' => $request->contact_zip_code,
+            ])
+        ]);
+
+		static::updateFaviconAndLogo($request);
+	}
+
+	public static function updateFaviconAndLogo(Request $request)
+	{
+
+        if ($favicon = $request->favicon_from_gallery) {
+			static::updateValueByKey('site-favicon', Image::byName($favicon)->url_cache);
+		} else if ($favicon = $request->file('favicon_from_local')) {
+			$faviconName = $favicon->getClientOriginalName();
+			$path = Storage::disk('uploads')->putFileAs('/', $favicon, $favicon->getClientOriginalName());
+			$image = Image::create([
+				'file_name' => $faviconName,
+				'url_asset' => url('uploads/' . $faviconName),
+				'url_cache' => url('imagecache/gallery/' . $faviconName)
+			]);
+			static::updateValueByKey('site-favicon', $image->url_cache);
+		}
+
+        if ($logo = $request->logo_from_gallery) {
+			static::updateValueByKey('site-logo', Image::byName($logo)->url_cache);
+		} else if ($logo = $request->file('logo_from_local')) {
+			$logoName = $logo->getClientOriginalName();
+			$path = Storage::disk('uploads')->putFileAs('/', $logo, $logo->getClientOriginalName());
+			$image = Image::create([
+				'file_name' => $logoName,
+				'url_asset' => url('uploads/' . $logoName),
+				'url_cache' => url('imagecache/gallery/' . $logoName)
+			]);
+			static::updateValueByKey('site-logo', $image->url_cache);
+		}
+	} 
 }
