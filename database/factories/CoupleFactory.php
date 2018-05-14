22680ba892d4257e2ea083da00673c708e7e96d8
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-14 21:22:40 +0800

    adding default name

--- database/factories/CoupleFactory.php
+++ database/factories/CoupleFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Couple::class, function (Faker $faker) {
     return [
+		'name' => $faker->firstNameMale . ' ' . $faker->lastName,
 		'mother' => $faker->firstNameFemale,
 		'father' => $faker->firstNameMale,
     ];
