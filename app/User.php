2759aa092e1378c71db673854f23dfcea19962b6
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-15 14:55:33 +0800

    define the relationships between role and user

--- app/User.php
+++ app/User.php
@@ -27,11 +27,42 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public function pages() {
+    public function pages()
+    {
         return $this->hasMany(Page::class);
     }
 
-    public function posts() {
+    public function posts()
+    {
         return $this->hasMany(Post::class);
     }
+
+    public function roles()
+    {
+		return $this->belongsToMany(Role::class);
+	}
+	
+	public function hasAnyRoles($roles)
+	{
+		if(is_array($roles)){
+			foreach ($roles as $role) {
+				if($this->hasRole($role)) {
+					return true; 
+				}
+			}
+		} else {
+			if($this->hasRole($roles)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public function hasRole($role)
+	{
+		if($this->roles()->whereName($role)->first()){
+			return true;
+		}
+		return false;
+	}
 }
