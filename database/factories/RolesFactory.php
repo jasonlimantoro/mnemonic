5b4d5124d5112bd286a76c66654c21b1a5b90544
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-15 13:17:03 +0800

    new factory

---
+++ database/factories/RolesFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Role;
+use Faker\Generator as Faker;
+
+$factory->define(Role::class, function (Faker $faker) {
+    return [
+		'name' => $faker->randomElement(['admin', 'author', 'user']),
+    ];
+});
+
+$factory->state(Role::class, 'admin', [
+	'name' => 'admin',
+	'label' => 'Administration Role' 
+]);
+
+$factory->state(Role::class, 'author', [
+	'name' => 'author',
+	'label' => 'Author Role' 
+]);
+
+$factory->state(Role::class, 'user', [
+	'name' => 'user',
+	'label' => 'User Role' 
+]);
