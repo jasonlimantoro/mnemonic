ec7f95cadbbf108d5c73b6b0652bfa723c48b443
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-17 03:46:43 +0800

    categorize states based on CRUD actions

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -13,15 +13,50 @@ class PermissionsTableSeeder extends Seeder
      */
     public function run()
     {
-		$permissions = factory(Permission::class, 10)->make();
+		$states = [
+            'carousel images',
+            'post',
+            'gallery',
+            'album',
+            'couple',
+            'event',
+            'bridesmaid_bestman',
+            'vendor',
+            'vendor_categories',
+            'rsvp',
+            'site_info',
+            'site_social',
+            'site_seo',
+            'user',
+            'role',
+		];
 
-		foreach ($permissions as $permission) {
-			repeat:
-			try {
-				$permission->save();
-			} catch (\Illuminate\Database\QueryException $e){
-				$permission = factory(Permission::class)->make();
-				goto repeat;
+		$complete = [
+			'carousel images',
+			'post',
+			'gallery',
+			'album',
+			'event',
+			'bridesmaid_bestman',
+			'vendor',
+			'vendor_categories',
+			'rsvp',
+			'user',
+			'role'
+		];
+
+		$incomplete = [
+			'couple',
+			'site_info',
+			'site_social',
+			'site_seo',
+		];
+
+		foreach ($states as $state) {
+			if(in_array($state, $complete)){
+				factory(Permission::class)->states('complete', $state)->create();
+			} else if (in_array($state, $incomplete)){
+				factory(Permission::class)->states('incomplete', $state)->create();
 			}
 		}
     }
