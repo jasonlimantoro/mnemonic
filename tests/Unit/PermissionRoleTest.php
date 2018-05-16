e814ed7f39921adc58d9a5af66bcbbd1ae8363ad
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-16 12:39:44 +0800

    using permissionrole factory for testing

---
+++ tests/Unit/PermissionRoleTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Role;
+use App\PermissionRole;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PermissionRoleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp();
+        factory(PermissionRole::class)->create([
+            'action' => [
+                'create' => false,
+                'read' => true,
+                'update' => true,
+                'delete' => false
+            ]
+        ]);
+    }
+
+    public function testGetAllowableActions()
+    {
+        $first = PermissionRole::first();
+        $role = Role::find($first->role_id);
+
+        $allowables = $role->allowables();
+
+        $diff = array_diff($allowables, ['read', 'update']);
+
+        $this->assertEmpty($diff);
+    }
+
+    public function testGetNotAllowableActions()
+    {
+        $first = PermissionRole::first();
+        $role = Role::find($first->role_id);
+
+        $notAllowables = $role->notAllowables();
+
+        $diff = array_diff($notAllowables, ['create', 'delete']);
+
+        $this->assertEmpty($diff);
+    }
+}
