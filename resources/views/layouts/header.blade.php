192fb1ebea8e46366b6e2314be49d90777a37f5f
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-19 00:51:04 +0800

    minor fix for navbar backend

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,45 +1,31 @@
 <nav class="navbar navbar-default navbar-backend">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <span class="glyphicon glyphicon-menu-hamburger sidebar-toggle" id="sidebarCollapse" role="button"></span>
-    </div>
+  <div class="navbar-header">
+    <span class="glyphicon glyphicon-menu-hamburger sidebar-toggle" id="sidebarCollapse" role="button"></span>
+  </div>
 
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav navbar-right">
-        <!-- Authentication Links -->
-        @guest
-        <li>
-          <a href="{{ route('login') }}">Login</a>
-        </li>
-        <li>
-          <a href="{{ route('register') }}">Register</a>
-        </li>
-        @else
-        <li class="dropdown">
-          <a dusk="profile-toggle" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-            {{ Auth::user()->name }}
-            <span class="caret"></span>
-          </a>
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav navbar-right">
+      <li class="dropdown">
+        <a dusk="profile-toggle" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+          {{ Auth::user()->name }}
+          <span class="caret"></span>
+        </a>
 
-          <ul class="dropdown-menu">
-            <li>
-              <a dusk="logout-link" href="{{ route('logout') }}" onclick="event.preventDefault();
-                  document.getElementById('logout-form').submit();">
-                Logout
-              </a>
-
-              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                {{ csrf_field() }}
-              </form>
-            </li>
-            <li>
-              <a href="/">Visit Website</a>
-            </li>
-          </ul>
-        </li>
-        @endguest
-      </ul>
-
-    </div>
+        <ul class="dropdown-menu">
+          <li>
+            <a dusk="logout-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+              Logout
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              {{ csrf_field() }}
+            </form>
+          </li>
+          <li>
+            <a href="/">Visit Website</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </nav>
