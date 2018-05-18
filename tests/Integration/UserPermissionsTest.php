a81b82def536363bbc774b6b73adcd2595021e78
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-18 21:47:23 +0800

    fix test

--- tests/Integration/UserPermissionsTest.php
+++ tests/Integration/UserPermissionsTest.php
@@ -2,12 +2,12 @@
 
 namespace Tests\Integration;
 
-use Tests\TestCase;
 use App\User;
 use App\Role;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use App\PermissionRole;
 use App\Permission;
+use App\PermissionRole;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserPermissionTest extends TestCase
 {
@@ -15,47 +15,44 @@ class UserPermissionTest extends TestCase
 
     public function testGetPermissible()
     {
-									
-		$admin = factory(Role::class)->states('admin')->create();
+        $admin = factory(Role::class)->states('admin')->create();
 
-		$albumPermission = factory(Permission::class)->states('album', 'complete')->create();
-		$postPermission = factory(Permission::class)->states('post', 'complete')->create();
+        $couplePermission = factory(Permission::class)->states('couple', 'incomplete')->create();
+        $postPermission = factory(Permission::class)->states('post', 'complete')->create();
 
-		$admintoAlbumPermissions = PermissionRole::create([
-			'permission_id' => $albumPermission->id,
-			'role_id' => $admin->id,
-			'action' => [
-				'create' => true,
-				'read' => true,
-				'update' => true,
-				'delete' => true
-			]
-		]);
+        $admintoCouplePermissions = PermissionRole::create([
+            'permission_id' => $couplePermission->id,
+            'role_id' => $admin->id,
+            'action' => [
+                'read' => true,
+                'update' => false,
+            ]
+        ]);
 
-		$admintoPostPermissions = PermissionRole::create([
-			'permission_id' => $postPermission->id,
-			'role_id' => $admin->id,
-			'action' => [
-				'create' => true,
-				'read' => true,
-				'update' => false,
-				'delete' => false
-			]
-		]);
+        $admintoPostPermissions = PermissionRole::create([
+            'permission_id' => $postPermission->id,
+            'role_id' => $admin->id,
+            'action' => [
+                'create' => true,
+                'read' => true,
+                'update' => false,
+                'delete' => false
+            ]
+        ]);
 
-		$user = factory(User::class)->create();
+        $user = factory(User::class)->create();
 
-		$user->role()->associate($admin);
-		$user->save();
+        $user->role()->associate($admin);
+        $user->save();
 
-		$this->assertEmpty(array_diff(
-			$user->permissibles('album'), 
-			['create', 'read', 'update', 'delete']
-		));
+        $this->assertEmpty(array_diff(
+            $user->permissibles('couple'),
+            ['read']
+        ));
 
-		$this->assertEmpty(array_diff(
-			$user->permissibles('post'), ['create', 'read']
-		));
-
+        $this->assertEmpty(array_diff(
+            $user->permissibles('post'),
+            ['create', 'read']
+        ));
     }
 }
