195684b6a248c25b4b80c6e15e979a81803b36b3
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-18 15:16:23 +0800

    add RSVP seeder

---
+++ database/seeds/RSVPTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\RSVP;
+
+class RSVPTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+		factory(RSVP::class, 5)->states('pending')->create();
+		factory(RSVP::class, 5)->states('confirmed')->create();
+		factory(RSVP::class, 5)->states('not coming')->create();
+    }
+}
