0f7f55e1017eb9b468bd0a1573aa247f0ad10402
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-19 00:29:19 +0800

    add welcoming message on dashboard

--- resources/views/backend/admin.blade.php
+++ resources/views/backend/admin.blade.php
@@ -2,19 +2,34 @@
 @section('content')
   <div class="row">
     <div class="col-md-12">
+			@component('layouts.breadcrumb')
+			@endcomponent
       @component('layouts.panel', [
-        'title' => 'Dashboard',
-      ])
-      
+				'title' => 'Dashboard',
+			])
         @slot('body')
-          @if (session('status'))
-          <div class="alert alert-success">
-            {{ session('status') }}
-          </div>
-          @endif
-          <p>
-            Welcome back {{ Auth::user()->name }} !
-          </p>
+          <p> Welcome back {{ auth()->user()->name }} ! </p>
+
+					<p> There are several tips to help you get started! </p>
+					<div class="row">
+						<div class="col-md-6">
+							<h3> Wedding </h3>
+							<ul>
+								<li><a href="{{ route('events.index')}} ">Manage Events</a></li>
+								<li><a href="{{ route('bridesmaid-bestmans.index')}}">Add Bridesmaid and Bestmen</a></li>
+								<li><a href="{{ route('rsvps.index')}}">Add your RSVP</a></li>
+								<li><a href="{{ route('vendors.index')}}">Add your vendor list</a></li>
+							</ul>
+						</div>
+						<div class="col-md-6">
+							<h3> Appearance </h3>
+							<ul>
+								<li><a href="{{ route('images.index') }}">Upload your photos</a></li>
+								<li><a href="{{ route('albums.index')}}">Organize your photos in album</a></li>
+								<li><a href="{{ route('carousel.images.index', ['page' => 1]) }}">Edit Main Carousel</a></li>
+							</ul>
+						</div>
+					</div>
         @endslot
       @endcomponent
     </div>
