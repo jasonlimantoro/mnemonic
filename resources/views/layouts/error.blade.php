e34d0233ff71c11c018426a7215b05e3612c40a1
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-20 23:45:55 +0800

    clean layouts

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -1,14 +1,12 @@
 @if($errors->any())
-    <div class="form-group">
-        {{--  <div class="col-md-offset-2 col-md-8">  --}}
-        <div class="alert alert-danger alert-dismissable">
-            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-        {{--  </div>  --}}
+  <div class="form-group">
+    <div class="alert alert-danger alert-dismissable">
+      <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+      <ul>
+        @foreach($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
     </div>
+  </div>
 @endif
--- resources/views/layouts/success.blade.php
+++ resources/views/layouts/success.blade.php
@@ -1,11 +1,8 @@
 @if( $flash = session('success_msg'))
-    <div class="form-group">
-        <div class="alert alert-success alert-dismissable">
-            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-            {{ $flash }}
-            {{--  @if(str_contains($flash, ['upload', 'image']))
-                <img src="{{ asset(session('fileDestination')) }}" alt="upload" class="img-responsive">
-            @endif  --}}
-        </div>
+  <div class="form-group">
+    <div class="alert alert-success alert-dismissable">
+      <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+      {{ $flash }}
     </div>
+  </div>
 @endif
