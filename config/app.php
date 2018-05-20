c9c50ba23a60ac9a80c08f81877c766fba8aca23
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-20 23:46:43 +0800

    recaptcha service providers

--- config/app.php
+++ config/app.php
@@ -180,6 +180,7 @@ return [
 		App\Providers\RouteServiceProvider::class,
 		App\Providers\FormMacroServiceProvider::class,
 		App\Providers\BladeServiceProvider::class,
+		Greggilbert\Recaptcha\RecaptchaServiceProvider::class,
 
     ],
 
@@ -231,7 +232,8 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 		'Image' => Intervention\Image\Facades\Image::class, 
 		'Form' => Collective\Html\FormFacade::class,
-		'Html' => Collective\Html\HtmlFacade::class
+		'Html' => Collective\Html\HtmlFacade::class,
+		'Recaptcha' => Greggilbert\Recaptcha\Facades\Recaptcha::class,
 
     ],
 
