f7ae06e433283b2f3a79ac4a85bfd3ecfbbe9389
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-20 17:18:12 +0800

    controller for onlineRSVP

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,59 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use JavaScript;
 use App\Page;
-use App\Post;
-use App\Image;
 use App\Album;
 use App\Event;
 use App\Couple;
-Use App\Carousel;
-use Carbon\Carbon;
-use App\Repositories\Posts;
-use Illuminate\Http\Request;
-use App\Http\Controllers\PostsController;
-use App\Http\Controllers\CarouselImagesController;
 use App\Vendor;
 use App\BridesBest;
+use Carbon\Carbon;
+use App\Repositories\Posts;
 
 class FrontendController extends Controller
 {
-    public function home(Posts $posts) {
-		$page = Page::find(1);
+    public function home(Posts $posts)
+    {
+        $page = Page::find(1);
         $carousel = $page->carousel;
-		$slides = $carousel->images;
-		
-		$posts =$posts->home()
-					->filter(request(['month', 'year']))
-					->get();
-			
+        $slides = $carousel->images;
+
+        $posts = $posts
+                    ->home()
+                    ->filter(request(['month', 'year']))
+                    ->get();
+
         return view('frontend.home', compact('posts', 'page', 'slides'));
     }
 
-    public function about() {
-		$couple = Couple::all();
+    public function about()
+    {
+        $couple = Couple::all();
         return view('frontend.about', compact('couple'));
     }
 
-    public function gallery() {
+    public function gallery()
+    {
         $albums = Album::filterId(request(['album']))->get();
 
         return view('frontend.gallery', compact('albums'));
-	}
-	
-	public function wedding()
-	{
-		$events = Event::all();
-		$groom = Couple::groom();
-		$bride = Couple::bride();
-		$bbs = BridesBest::all();
-		$vendors = Vendor::all();
+    }
 
-		return view('frontend.wedding', compact('events', 'groom', 'bride', 'bbs', 'vendors'));
-	}
+    public function wedding()
+    {
+        $events = Event::all();
+        $groom = Couple::groom();
+        $bride = Couple::bride();
+        $bbs = BridesBest::all();
+        $vendors = Vendor::all();
 
-	public function onlineRSVP()
-	{
-			
-	}
+        return view('frontend.wedding', compact('events', 'groom', 'bride', 'bbs', 'vendors'));
+    }
+
+    public function onlineRSVP()
+    {
+		$wedding = Event::byName('wedding');
+		$weddingDate = Event::getDateTimeObjectAttribute($wedding->datetime);
+		$isFuture = Carbon::now()->diffInSeconds($weddingDate, false) > 0;
+		JavaScript::put([
+			'weddingDate' => $weddingDate,
+		]);
+        return view('frontend.online-rsvp', compact('wedding', 'isFuture'));
+    }
 }
