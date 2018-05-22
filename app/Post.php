22bdf6296a9b14fdec64da532ae81f171bc23821
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-22 21:56:50 +0800

    define a new relationships with image

--- app/Post.php
+++ app/Post.php
@@ -1,41 +1,47 @@
 <?php
 
 namespace App;
+
 use Carbon\Carbon;
 use App\Traits\FiltersSearch;
 
-
 class Post extends Model
 {
-	use FiltersSearch;
-    public function user() { // $post->user->name
+    use FiltersSearch;
+
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function page() { //$post->page
+    public function page()
+    {
         return $this->belongsTo(Page::class);
-	}
+    }
 
-	public function scopeFilter($query, $filters)
-	// frontend
-	{
-		if($filters && $month = $filters['month'])
-		{
-			$query->whereMonth('created_at', Carbon::parse($month)->month);
-		}
-		if($filters && $year = $filters['year'])
-		{
-			$query->whereYear('created_at', $year);
-		}
-	}
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
 
-	public static function archives()
-	{
-		return static::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
-					->groupBy('year', 'month')
-					->orderByRaw('min(created_at) desc')
-					->where('page_id', 1)
-					->get()
-					->toArray();
-	}
+    public function scopeFilter($query, $filters)
+    // frontend
+    {
+        if ($filters && $month = $filters['month']) {
+            $query->whereMonth('created_at', Carbon::parse($month)->month);
+        }
+        if ($filters && $year = $filters['year']) {
+            $query->whereYear('created_at', $year);
+        }
+    }
+
+    public static function archives()
+    {
+        return static::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
+                    ->groupBy('year', 'month')
+                    ->orderByRaw('min(created_at) desc')
+                    ->where('page_id', 1)
+                    ->get()
+                    ->toArray();
+    }
 }
