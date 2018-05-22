4916a0bc3f7e4e27f45cf79645ec0c0bc50b5331
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-22 14:50:34 +0800

    add title

--- resources/views/frontend/gallery.blade.php
+++ resources/views/frontend/gallery.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
   <div class="web-container">		
+		<div class="row page-title font-theme">
+			<h1 class="color-theme">Our Gallery</h1>
+		</div>
 		<div class="row row-center">
 			<div class="col-md-10 col-center">
 				<div class="__react-root" id="AlbumSlider"></div>
