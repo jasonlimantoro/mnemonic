330b495ca0a76e3716b46768298bf5d4c9ac889d
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-23 18:05:21 +0800

    make the slider responsive

--- resources/assets/js/components/Slider.js
+++ resources/assets/js/components/Slider.js
@@ -75,8 +75,22 @@ export class AlbumSlider extends React.Component {
       useTransform: false,
       infinite: true,
       speed: 500,
-      slidesToShow: 2,
-      slidesToScroll: 1
+      slidesToShow: 3,
+			slidesToScroll: 1,
+			responsive : [
+				{
+					breakpoint: 1024,
+					settings : {
+						slidesToShow : 2,
+					}
+				},
+				{
+					breakpoint: 767,
+					settings : {
+						slidesToShow : 1,
+					}
+				}
+			]
     };
 
     let slides = this.state.albums.map(album => {
