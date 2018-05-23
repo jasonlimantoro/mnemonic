eaa796853dfa00375b234b29d49a6cea11103067
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-24 00:25:14 +0800

    new component UnauthorizedButton

--- resources/assets/js/components/Button.js
+++ resources/assets/js/components/Button.js
@@ -2,43 +2,51 @@ import React from "react";
 import { Button } from "react-bootstrap";
 
 export const PrimaryButton = (props) => {
-    return (
-        <Button dusk={props.dusk} type={props.type} bsStyle="primary" onClick={props.onClick}>
-            {props.text} 
-        </Button>
-    )
+  return (
+    <Button dusk={props.dusk} type={props.type} bsStyle="primary" onClick={props.onClick}>
+      {props.text} 
+    </Button>
+  )
 }
 
 export const SuccessButton = (props) => {
-    return (
-        <div>
-            <Button type={props.type} bsStyle="success" onClick={props.onClick}>
-                {props.text}
-            </Button>
-        </div>
-    )
+  return (
+    <div>
+      <Button type={props.type} bsStyle="success" onClick={props.onClick}>
+        {props.text}
+      </Button>
+    </div>
+  )
 }
 
 export const InfoButton = (props) => {
-    return (
-        <Button type={props.type} bsStyle="info" onClick={props.onClick}>
-            {props.text} 
-        </Button>
-    )
+  return (
+    <Button type={props.type} bsStyle="info" onClick={props.onClick}>
+      {props.text} 
+    </Button>
+  )
 }
 
 export const WarningButton = (props) => {
-    return (
-        <Button type={props.type} bsStyle="warning" onClick={props.onClick}>
-            {props.text} 
-        </Button>
-    )
+  return (
+    <Button type={props.type} bsStyle="warning" onClick={props.onClick}>
+      {props.text} 
+    </Button>
+  )
 }
 
 export const DangerButton = (props) => {
-    return (
-        <Button type={props.type} bsStyle="danger" onClick={props.onClick}>
-            {props.text} 
-        </Button>
-    )
+  return (
+    <Button type={props.type} bsStyle="danger" onClick={props.onClick}>
+      {props.text} 
+    </Button>
+  )
+}
+
+export const UnauthorizedButton = (props) => {
+  return (
+		<Button type="button" onClick={props.onClick} disabled>
+			Unauthorized
+		</Button>
+  );
 }
