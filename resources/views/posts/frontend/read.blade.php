5ceee59280640d56f79fe14865bee856db8c9ff4
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-29 10:57:53 +0700

    wrap in web-container

--- resources/views/posts/frontend/read.blade.php
+++ resources/views/posts/frontend/read.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.master')
 
 @section('content')
-  <div class="col-md-8 blog-post">
-    <h2>{{ $post->title }}</h2>
-    <p>{!! $post->description !!}</p>
-  </div>
-  <div class="col-md-4">
-    <h1>Archives</h1>
-    @include('layouts.archives')
+  <div class="web-container">
+    <div class="row">
+			<div class="col-md-8 blog-post">
+				<h2>{{ $post->title }}</h2>
+				<p>{!! $post->description !!}</p>
+			</div>
+			<div class="col-md-4">
+				<h1>Archives</h1>
+				@include('layouts.archives')
+			</div>
+		</div>
   </div>
 @endsection
