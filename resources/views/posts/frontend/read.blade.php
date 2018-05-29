ca63c73653be9e14411991c4ea7ebcb386ae918c
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-29 11:20:31 +0700

    include image in front.posts.read

--- resources/views/posts/frontend/read.blade.php
+++ resources/views/posts/frontend/read.blade.php
@@ -2,14 +2,23 @@
 
 @section('content')
   <div class="web-container">
-    <div class="row">
-			<div class="col-md-8 blog-post">
-				<h2>{{ $post->title }}</h2>
-				<p>{!! $post->description !!}</p>
-			</div>
-			<div class="col-md-4">
-				<h1>Archives</h1>
-				@include('layouts.archives')
+		<div class="container">
+			@isset($post->image)
+				<div class="row">
+					<div class="col-md-12">
+						<img src="{{ $post->image->url_cache }}" alt="post-image" class="img-responsive">
+					</div>
+				</div>
+			@endisset
+	    <div class="row">
+				<div class="col-md-8 blog-post">
+					<h2>{{ $post->title }}</h2>
+					<p>{!! $post->description !!}</p>
+				</div>
+				<div class="col-md-4">
+					<h1>Archives</h1>
+					@include('layouts.archives')
+				</div>
 			</div>
 		</div>
   </div>
