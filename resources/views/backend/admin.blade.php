372b774b3a9bbe8b8f7ef74ce3716f3265afbe8f
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-05-30 12:41:47 +0700

    apply authorization to dashboard page

--- resources/views/backend/admin.blade.php
+++ resources/views/backend/admin.blade.php
@@ -15,18 +15,34 @@
 						<div class="col-md-6">
 							<h3> Wedding </h3>
 							<ul>
-								<li><a href="{{ route('events.index')}} ">Manage Events</a></li>
-								<li><a href="{{ route('bridesmaid-bestmans.index')}}">Add Bridesmaid and Bestmen</a></li>
-								<li><a href="{{ route('rsvps.index')}}">Add your RSVP</a></li>
-								<li><a href="{{ route('vendors.index')}}">Add your vendor list</a></li>
+								@can('read', App\Event::class)
+									<li><a href="{{ route('events.index')}}">Manage Events</a></li>
+								@endcan
+
+								@can('read', App\BridesBest::class)
+									<li><a href="{{ route('bridesmaid-bestmans.index')}}">Add Bridesmaid and Bestmen</a></li>
+								@endcan
+
+								@can('read', App\RSVP::class)
+									<li><a href="{{ route('rsvps.index')}}">Add your RSVP</a></li>
+								@endcan
+
+								@can('read', App\Vendor::class)
+									<li><a href="{{ route('vendors.index')}}">Add your vendor list</a></li>
+								@endcan
 							</ul>
 						</div>
 						<div class="col-md-6">
 							<h3> Appearance </h3>
 							<ul>
-								<li><a href="{{ route('images.index') }}">Upload your photos</a></li>
-								<li><a href="{{ route('albums.index')}}">Organize your photos in album</a></li>
-								<li><a href="{{ route('carousel.images.index', ['page' => 1]) }}">Edit Main Carousel</a></li>
+								@can('manage-gallery')
+									<li><a href="{{ route('images.index') }}">Upload your photos</a></li>
+									<li><a href="{{ route('albums.index')}}">Organize your photos in album</a></li>
+								@endcan
+
+								@can('read-carousel-image')
+									<li><a href="{{ route('carousel.images.index', ['page' => 1]) }}">Edit Main Carousel</a></li>
+								@endcan
 							</ul>
 						</div>
 					</div>
