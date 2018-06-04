b713519e156e9106f39ca4e08b8a38ac40f1044d
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-06-05 01:01:09 +0700

    fix heading font, and add the bb-slider react component

--- resources/views/frontend/wedding.blade.php
+++ resources/views/frontend/wedding.blade.php
@@ -1,42 +1,40 @@
 @extends('layouts.master')
 
 @section('content')
-	<div class="wedding-day-container text-center">
+	<div class="container-fluid text-center">
 		{{-- Overview Section --}}
 		<div class="row wedding-day-overview">
-			<div class="container">
+			<div class="col-xs-12">
+				<div class="page-title">
+					<h1 class="color-theme font-theme">Wedding Day</h1>
+				</div>
+				@isset($embed)
+					<div class="embed-responsive embed-responsive-16by9">
+						<iframe class="embed-responsive-item" src="http://www.youtube.com/embed/{{ $embed->id }}"></iframe>
+					</div>
+				@endisset
 				<div class="col-xs-12">
-					<div class="page-title">
-						<h1 class="color-theme">Wedding Day</h1>
+					<h2>We would like to invite you to our marriage celebration</h2>
+				</div>
+				
+				<div class="col-sm-5 col-xs-12">
+					<div class="vip-container">
+						<h3 class="font-theme vip-name">{{ $groom->name }}</h3>
+						<span class="font-theme">Son of</span> <br>
+						<p>{{ $groom->father }} &</p>
+						<p>{{ $groom->mother }}</p>
+						<img src="{{ $groom->image->url_cache }}" alt="vip" class="img-responsive inline-block">
 					</div>
-					@isset($embed)
-						<div class="embed-responsive embed-responsive-16by9">
-							<iframe class="embed-responsive-item" src="http://www.youtube.com/embed/{{ $embed->id }}"></iframe>
-						</div>
-					@endisset
-					<div class="col-xs-12">
-						<h2 class="font-theme">We would like to invite you to our marriage celebration</h2>
-					</div>
-					
-					<div class="col-sm-5 col-xs-12">
-						<div class="groom-container">
-							<h3 class="font-theme groom-name">{{ $groom->name }}</h3>
-							<span class="font-theme">Son of</span> <br>
-							<p> {{ $groom->father }} </p>
-							<p> {{ $groom->mother }} </p>
-							<img src="{{ $groom->image->url_cache }}" alt="groom" class="img-responsive inline-block">
-						</div>
-					</div>
-					<div class="col-sm-2 hidden-xs link">&</div>
-		
-					<div class="col-sm-5 col-xs-12">
-						<div class="bride-container">
-							<h3 class="font-theme bride-name">{{ $bride->name }}</h3>
-							<span class="font-theme">Daughter of</span> <br>
-							<p> {{ $bride->father }} </p>
-							<p> {{ $bride->mother }} </p>
-							<img src="{{ $bride->image->url_cache }}" alt="groom" class="img-responsive inline-block">
-						</div>
+				</div>
+				<div class="col-sm-2 hidden-xs link">&</div>
+	
+				<div class="col-sm-5 col-xs-12">
+					<div class="vip-container">
+						<h3 class="font-theme vip-name">{{ $bride->name }}</h3>
+						<span class="font-theme">Daughter of</span> <br>
+						<p>{{ $bride->father }} &</p>
+						<p>{{ $bride->mother }}</p>
+						<img src="{{ $bride->image->url_cache }}" alt="vip" class="img-responsive inline-block">
 					</div>
 				</div>
 			</div>
@@ -79,25 +77,10 @@
 			<div class="wedding-day-bb-message">
 				<p><i>Thank you for your support on our wedding</i></p>
 			</div>
-			<div class="row">
-				@foreach ($bbs as $bb)
-					<div class="col-sm-3 col-xs-12 col-center wedding-day-bb-container">
-					@if ($bb->image)
-						<div class="wedding-day-bb-image">
-							<img src="{{ $bb->image->url_cache }}" alt="bb" class="img-responsive">
-						</div>	
-					@endif
-						<strong>{{ $bb->name }}</strong><br>
-						<i>{!! $bb->testimony !!}</i><br>
-						<div class="wedding-day-bb-ig">
-							<div class="col-xs-12 col-center">
-								<a href="https://instagram.com/{{ $bb->ig_account}}">
-									<img src="/images/instagram-logo.png" alt="ig" class="img-responsive" width="32px">
-								</a>
-							</div>
-						</div>
-					</div>
-				@endforeach
+			
+			@include('jsvar')
+			<div class="col-xs-10 col-center">
+				<div class="__react-root" id="BridesBestSlider"></div>
 			</div>
 		</div>
 
