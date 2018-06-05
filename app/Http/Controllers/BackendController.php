73a91ab28fd50340a1e89f15e3ba956b6682cd47
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-06-06 03:33:50 +0700

    remove unused imports

--- app/Http/Controllers/BackendController.php
+++ app/Http/Controllers/BackendController.php
@@ -2,10 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-use Illuminate\Support\Facades\DB;
-
 class BackendController extends Controller
 {
     public function admin()
