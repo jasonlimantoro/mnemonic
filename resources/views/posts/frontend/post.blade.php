39a0927a49ac441a7f19bf260bf73108c0789f20
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-06-06 02:01:14 +0700

    fix styles

--- resources/views/posts/frontend/post.blade.php
+++ resources/views/posts/frontend/post.blade.php
@@ -1,6 +1,13 @@
 <h1 class="color-theme post-title">{{ $post->title }}</h1>
-<p> {!! $post->description !!} </p>
-<a href="{{ route('front.posts.read', ['post' => $post->id ]) }}">
-  Read More
-</a>
+@isset($post->image)
+  <div class="post-image">
+    <img src="{{ $post->image->url_cache }}" alt="post-image" class="img-responsive">
+  </div>
+@endisset
+<p> {!! str_limit(strip_tags($post->description, "<h1><h2><h3><h4><p><i><span><b><img>"), 1000, '...') !!} </p>
+<p>
+  <a href="{{ route('front.posts.read', ['post' => $post->id ]) }}">
+    Read More
+  </a>
+</p>
 
--- resources/views/posts/frontend/read.blade.php
+++ resources/views/posts/frontend/read.blade.php
@@ -2,22 +2,22 @@
 
 @section('content')
 	<div class="container">
-		<h1 class="color-theme text-center post-title">{{ $post->title }}</h1>
-		@isset($post->image)
-			<div class="row">
-				<div class="col-md-12">
-					<img src="{{ $post->image->url_cache }}" alt="post-image" class="img-responsive">
-				</div>
-			</div>
-		@endisset
-    <div class="row">
-			<div class="col-md-8 blog-post">
-				<p>{!! $post->description !!}</p>
-			</div>
-			<div class="col-md-4">
-				<h1>Archives</h1>
-				@include('layouts.archives')
-			</div>
-		</div>
-	</div>
+    <div class="post-container">
+      <h1 class="color-theme text-center post-title">{{ $post->title }}</h1>
+      @isset($post->image)
+        <div class="post-image">
+          <img src="{{ $post->image->url_cache }}" alt="post-image" class="img-responsive">
+        </div>
+      @endisset
+      <div class="row">
+        <div class="col-md-8 blog-post">
+          <p>{!! $post->description !!}</p>
+        </div>
+        <div class="col-md-4">
+          <h1>Archives</h1>
+          @include('layouts.archives')
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
