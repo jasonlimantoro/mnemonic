622dea1ac0b55a9bbb7f359dfedb7b268185bbb2
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-06-06 13:26:30 +0700

    declare public properties on base controller

--- app/Http/Controllers/GenericController.php
+++ app/Http/Controllers/GenericController.php
@@ -5,6 +5,22 @@ namespace App\Http\Controllers;
 
 abstract class GenericController extends Controller
 {
+
+    /**
+     * Filter name for Image Intervention
+     *
+     * @var string
+     */
+    protected $filter;
+
+
+    /**
+     * Filter class name of the corresponding filter
+     *
+     * @var string
+     */
+    protected $filterClass;
+
 	/**
 	 * Send flash message after every action
 	 *
