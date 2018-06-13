e9ff66506f93bbb9a6400e6c4949f9fe6d015b6b
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-06-12 22:52:49 -0400

    fix test for presenter methods

--- tests/Feature/GroupEventByDateTest.php
+++ tests/Feature/GroupEventByDateTest.php
@@ -12,47 +12,19 @@ class GroupEventByDateTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testGroupEventByDate()
-    {
-        // Now (2)
-        $first = factory(Event::class, 2)->create([
-            'datetime' => now(),
-        ]);
-
-        // Yesterday (1)
-        $second = factory(Event::class)->create([
-            'datetime' => Carbon::now()->subDay()
-        ]);
-
-        $grouped = Event::groupByDate();
-
-        $expected = [
-            [
-                'datetime' => $first->first()->datetime,
-                'occurrences' => 2,
-            ],
-            [
-                'datetime' => $second->datetime,
-                'occurrences' => 1,
-            ],
-        ];
-
-        $this->assertEquals($expected, $grouped);
-    }
-
     public function testGetDistinctDate()
     {
         // Now (2)
         $first = factory(Event::class, 2)->create([
-            'datetime' => now(),
+            'datetime' => Carbon::today(),
         ]);
 
         // Yesterday (1)
         $second = factory(Event::class)->create([
-            'datetime' => Carbon::now()->subDay()
+            'datetime' => Carbon::yesterday(),
         ]);
 
-        $dates = Event::getDistinctDate();
+        $dates = Event::process()->getDistinctDate();
 
         $expected = [$first->first()->datetime, $second->datetime];
 
@@ -66,34 +38,38 @@ class GroupEventByDateTest extends TestCase
             'datetime' => Carbon::today(),
         ]);
 
+        $firstPretty = $first->first()->present()->prettyDatetime;
+
         // Yesterday (1)
         $second = factory(Event::class)->create([
             'datetime' => Carbon::yesterday()
         ]);
+        $secondPretty = $second->present()->prettyDatetime;
 
-        $dates = [$first->first()->datetime, $second->datetime];
 
-        $processed = Event::processDistinctDate($dates);
+        $processed = Event::process()->displayEventsGroupByDate();
+
 
         $expected = [
-            $first->first()->datetime => $first->toArray(),
-            $second->datetime => [$second->toArray()],
+            $firstPretty => $first,
+            $secondPretty => collect([$second]),
         ];
+
 
         // Today (2)
         $this->assertEquals(
-            $expected[$first->first()->datetime][0]['id'],
-            $processed[$first->first()->datetime][0]['id']
+            $expected[$firstPretty][0]->id,
+            $processed[$firstPretty][0]->id
         );
         $this->assertEquals(
-            $expected[$first->first()->datetime][1]['id'],
-            $processed[$first->first()->datetime][1]['id']
+            $expected[$firstPretty][1]->id,
+            $processed[$firstPretty][1]->id
         );
 
         // Yesterday (1)
         $this->assertEquals(
-            $expected[$second->datetime][0]['id'],
-            $processed[$second->datetime][0]['id']
+            $expected[$secondPretty]->first()->id,
+            $processed[$secondPretty]->first()->id
         );
     }
 }
