7cda5d402ab9af953e7bd3f344408eb8f63e3c2a
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-06-24 12:18:58 -0700

    Simplify button component

--- resources/assets/js/backend/components/Button.js
+++ resources/assets/js/backend/components/Button.js
@@ -1,47 +1,55 @@
 import React from "react";
 import {Button} from "react-bootstrap";
 
-export const PrimaryButton = ({ onClick, children, ...rest }) => {
+export const PrimaryButton = ({onClick, children, ...rest}) => {
   return (
-    <Button bsStyle="primary" onClick={onClick} {...rest}>
+    <Button bsStyle="primary" {...rest}>
       {children}
     </Button>
   );
 };
 
-export const SuccessButton = ({ onClick, children, ...rest}) => {
+export const SuccessButton = ({onClick, children, ...rest}) => {
   return (
-    <Button bsStyle="success" onClick={onClick} {...rest}>
+    <Button bsStyle="success" {...rest}>
       {children}
     </Button>
   );
 };
 
-export const InfoButton = ({ onClick, children, ...rest }) => {
+export const InfoButton = ({children, ...rest}) => {
   return (
-    <Button bsStyle="info" onClick={onClick} {...rest}>
+    <Button bsStyle="info" {...rest}>
       {children}
     </Button>
   );
 };
 
-export const WarningButton = ({ onClick, children, ...rest }) => {
+export const WarningButton = ({children, ...rest}) => {
   return (
-    <Button bsStyle="warning" onClick={onClick} {...rest}>
+    <Button bsStyle="warning" {...rest}>
       {children}
     </Button>
   );
 };
 
-export const DangerButton = ({ onClick, children, ...rest }) => {
+export const DangerButton = ({children, ...rest}) => {
   return (
-    <Button bsStyle="danger" onClick={onClick} {...rest}>
+    <Button bsStyle="danger" {...rest}>
       {children}
     </Button>
   );
 };
 
-export const UnauthorizedButton = ({ onClick,...rest }) => {
+export const LinkButton = ({submit, children, ...rest}) => {
+  return (
+    <Button bsStyle="link" type={submit ? 'submit' : 'button'} {...rest}>
+      {children}
+    </Button>
+  );
+};
+
+export const UnauthorizedButton = ({onClick, ...rest}) => {
   return (
     <Button onClick={onClick} disabled {...rest}>
       Unauthorized
