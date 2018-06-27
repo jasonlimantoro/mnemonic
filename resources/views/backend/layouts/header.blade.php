3e72ce98bf07f4ccd7ddb23efbd020c512f09373
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-06-26 18:25:13 -0700

    fix style for backend header

--- resources/views/backend/layouts/header.blade.php
+++ resources/views/backend/layouts/header.blade.php
@@ -1,28 +1,25 @@
-<nav class="navbar navbar-default navbar-backend">
-  <div class="navbar-header">
-    <span class="glyphicon glyphicon-menu-hamburger sidebar-toggle" id="sidebarCollapse" role="button"></span>
-  </div>
-
-  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+<nav class="navbar navbar-static-top">
+  <span class="glyphicon glyphicon-menu-hamburger sidebar-toggle" id="sidebarCollapse" role="button"></span>
+  <div class="navbar-custom-menu pull-right">
     <ul class="nav navbar-nav navbar-right">
-      <li class="dropdown">
-        <a dusk="profile-toggle" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+      <li>
+        <a dusk="profile-toggle" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="true"
+           aria-haspopup="true">
           {{ Auth::user()->name }}
-          <span class="caret"></span>
         </a>
 
         <ul class="dropdown-menu">
           <li>
+            <a href="{{ route('front.index') }}" target="_blank">Visit Website</a>
+          </li>
+          <li>
             <a dusk="logout-link" href="{{ route('logout') }}" onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();">
+									document.getElementById('logout-form').submit();">
               Logout
             </a>
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
               {{ csrf_field() }}
             </form>
-          </li>
-          <li>
-            <a href="{{ route('front.index') }}" target="_blank">Visit Website</a>
           </li>
         </ul>
       </li>
