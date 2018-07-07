ad33ebd6d85125dc2f531bcf2ce1456a960c99c8
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-07-08 00:07:35 +0700

    Presenter for Post Model

--- app/Post.php
+++ app/Post.php
@@ -2,10 +2,15 @@
 
 namespace App;
 
-use Carbon\Carbon;
+use App\Traits\Presentable;
+use App\Presenters\PostPresenter;
 
 class Post extends Model
 {
+	use Presentable;
+	
+	protected $presenter = PostPresenter::class;
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -19,26 +24,5 @@ class Post extends Model
     public function image()
     {
         return $this->morphOne(Image::class, 'imageable');
-    }
-
-    public function scopeFilter($query, $filters)
-    // frontend
-    {
-        if ($filters && $month = $filters['month']) {
-            $query->whereMonth('created_at', Carbon::parse($month)->month);
-        }
-        if ($filters && $year = $filters['year']) {
-            $query->whereYear('created_at', $year);
-        }
-    }
-
-    public static function archives()
-    {
-        return static::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
-                    ->groupBy('year', 'month')
-                    ->orderByRaw('min(created_at) desc')
-                    ->where('page_id', 1)
-                    ->get()
-                    ->toArray();
     }
 }
