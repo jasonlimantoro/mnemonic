da096a5dc8716f7db2e0b26f8c3177fa1c3affd1
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-07-18 11:30:37 +0700

    Fix repositories method

--- app/Repositories/Albums.php
+++ app/Repositories/Albums.php
@@ -6,32 +6,59 @@ use App\Album;
 
 class Albums 
 {
-	public static function all()
+    /**
+     * @return Album|\Illuminate\Database\Query\Builder
+     */
+    public static function all()
 	{
 		return Album::latest();
 	}
 
-	public static function withImages()
+    /**
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public static function withImages()
 	{
-		return Album::with('images')->oldest()->get();
-	}
-	
-	public static function categorized()
-	{
-		return static::all()->where('name', '!=', 'Uncategorized');
+		return Album::with('images');
 	}
 
-	public static function uncategorized()
-	{
-		return Album::where('name', 'Uncategorized')->first();
+    /**
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public static function withoutImages()
+    {
+        return Album::without('images');
 	}
 
-	public static function toArray()
+    /**
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public static function categorized()
+	{
+		return static::withoutImages()->where('name', '!=', 'Uncategorized');
+	}
+
+
+    /**
+     * @return Album|null
+     */
+    public static function uncategorized()
+	{
+		return static::withoutImages()->where('name', 'Uncategorized')->first();
+	}
+
+    /**
+     * @return array
+     */
+    public static function toArray()
 	{
 		return static::all()->pluck('name', 'id')->toArray();
 	}
 
-	public static function filtered()
+    /**
+     * @return mixed
+     */
+    public static function filtered()
     {
         return static::categorized()->filtersSearch(request(['search', 'order', 'method']));
     }
