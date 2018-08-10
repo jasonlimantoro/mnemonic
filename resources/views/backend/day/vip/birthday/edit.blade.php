bf3991ef7d58d4445b691b5c1880f9e2b749c1c7
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-08-10 12:57:34 +0800

    Fix vip backend

--- resources/views/backend/day/vip/birthday/edit.blade.php
+++ resources/views/backend/day/vip/birthday/edit.blade.php
@@ -1,35 +1,31 @@
 @extends('backend.layouts.master')
 
 @section('content')
-  @component('backend.layouts.breadcrumb', ['current' => 'Couple'])
+  @component('backend.layouts.breadcrumb', ['current' => 'VIP'])
   @endcomponent
   @component('backend.layouts.panel', [
-    'title' => 'Couple Information'
+    'title' => 'Birthday Person Information' 
   ])
 
     @slot('body')
-      {{ Form::open(['route' => 'vip.update', 'method' => 'PATCH', 'enctype' => 'multipart/form-data']) }}
-      @foreach($vips as $vip)
-        @php
-          $role = $vip->gender === 'male' ? 'groom' : 'bride';
-        @endphp
-        <div class="col-xs-12 col-md-6">
+      {{ Form::open(['route' => 'vip.update', 'method' => 'PATCH']) }}
+        <div class="col-xs-12">
           {{-- name field --}}
           <div class="form-group">
-            {{ Form::label($role . '_name', 'Name:') }}
-            {{ Form::text($role . '_name', $vip->name, ['class' => 'form-control', 'placeholder' => 'Enter Name']) }}
+            {{ Form::label('vip_name', 'Name:') }}
+            {{ Form::text('vip_name', $vip->birthday_person->name, ['class' => 'form-control', 'placeholder' => 'Enter Name']) }}
           </div>
 
           {{-- father field --}}
           <div class="form-group">
-            {{ Form::label($role . '_father', 'Father:') }}
-            {{ Form::text($role . '_father', $vip->father, ['class' => 'form-control', 'placeholder' => 'Enter Father']) }}
+            {{ Form::label('vip_father', 'Father:') }}
+            {{ Form::text('vip_father', $vip->birthday_person->father, ['class' => 'form-control', 'placeholder' => 'Enter Father']) }}
           </div>
 
           {{-- mother field --}}
           <div class="form-group">
-            {{ Form::label($role . '_mother', 'Mother:') }}
-            {{ Form::text($role . '_mother', $vip->mother, ['class' => 'form-control', 'placeholder' => 'Enter Mother']) }}
+            {{ Form::label('vip_mother', 'Mother:') }}
+            {{ Form::text('vip_mother', $vip->birthday_person->mother, ['class' => 'form-control', 'placeholder' => 'Enter Mother']) }}
           </div>
 
 
@@ -38,8 +34,8 @@
               <strong>Current Image</strong>
             </p>
             <div class="current-image">
-              @isset($vip->image)
-                <img src="{{ url('/imagecache/vip/'. $vip->image) }}" alt="vip" class="img-responsive">
+              @isset($vip->birthday_person->image)
+                <img src="{{ url('/imagecache/vip/'. $vip->birthday_person->image) }}" alt="vip" class="img-responsive">
               @else
                 <p class="no-current-image">No image uploaded</p>
               @endisset
@@ -47,14 +43,13 @@
           </div>
           <div class="form-group">
             <div data-component="FancyInput"
-                 data-prop-input-name="{{ $role }}_image"
-                 data-prop-initial-input-value="{{ isset($vip->image) ? $vip->image : '' }}"
+                 data-prop-input-name="vip_image"
+                 data-prop-initial-input-value="{{ isset($vip->birthday_person->image) ?: $vip->birthday_person->image }}"
                  data-prop-template="vip"
             >
             </div>
           </div>
         </div>
-      @endforeach
 
       <div class="col-xs-12">
         {{-- Submit Button --}}
---
+++ resources/views/backend/day/vip/wedding/edit.blade.php
@@ -0,0 +1,115 @@
+@extends('backend.layouts.master')
+
+@section('content')
+  @component('backend.layouts.breadcrumb', ['current' => 'VIP'])
+  @endcomponent
+  @component('backend.layouts.panel', [
+    'title' => 'Couple Information'
+  ])
+
+    @slot('body')
+      {{ Form::open(['route' => 'vip.update', 'method' => 'PATCH', 'enctype' => 'multipart/form-data']) }}
+      @php
+        $role = 'groom';
+      @endphp
+      <div class="col-xs-12 col-md-6">
+        {{-- name field --}}
+        <div class="form-group">
+          {{ Form::label($role . '_name', 'Name:') }}
+          {{ Form::text($role . '_name', $vip->groom->name, ['class' => 'form-control', 'placeholder' => 'Enter Name']) }}
+        </div>
+
+        {{-- father field --}}
+        <div class="form-group">
+          {{ Form::label($role . '_father', 'Father:') }}
+          {{ Form::text($role . '_father', $vip->groom->father, ['class' => 'form-control', 'placeholder' => 'Enter Father']) }}
+        </div>
+
+        {{-- mother field --}}
+        <div class="form-group">
+          {{ Form::label($role . '_mother', 'Mother:') }}
+          {{ Form::text($role . '_mother', $vip->groom->mother, ['class' => 'form-control', 'placeholder' => 'Enter Mother']) }}
+        </div>
+
+
+        <div class="form-group">
+          <p>
+            <strong>Current Image</strong>
+          </p>
+          <div class="current-image">
+            @isset($vip->groom->image)
+              <img src="{{ url('/imagecache/vip/'. $vip->groom->image) }}" alt="vip" class="img-responsive">
+            @else
+              <p class="no-current-image">No image uploaded</p>
+            @endisset
+          </div>
+        </div>
+        <div class="form-group">
+          <div data-component="FancyInput"
+               data-prop-input-name="{{ $role }}_image"
+               data-prop-initial-input-value="{{ isset($vip->groom->image) ?: $vip->groom->image }}"
+               data-prop-template="vip"
+          >
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xs-12 col-md-6">
+        @php
+          $role = 'bride'
+        @endphp
+        {{-- name field --}}
+        <div class="form-group">
+          {{ Form::label($role . '_name', 'Name:') }}
+          {{ Form::text($role . '_name', $vip->bride->name, ['class' => 'form-control', 'placeholder' => 'Enter Name']) }}
+        </div>
+
+        {{-- father field --}}
+        <div class="form-group">
+          {{ Form::label($role . '_father', 'Father:') }}
+          {{ Form::text($role . '_father', $vip->bride->father, ['class' => 'form-control', 'placeholder' => 'Enter Father']) }}
+        </div>
+
+        {{-- mother field --}}
+        <div class="form-group">
+          {{ Form::label($role . '_mother', 'Mother:') }}
+          {{ Form::text($role . '_mother', $vip->bride->mother, ['class' => 'form-control', 'placeholder' => 'Enter Mother']) }}
+        </div>
+
+
+        <div class="form-group">
+          <p>
+            <strong>Current Image</strong>
+          </p>
+          <div class="current-image">
+            @isset($vip->bride->image)
+              <img src="{{ url('/imagecache/vip/'. $vip->bride->image) }}" alt="vip" class="img-responsive">
+            @else
+              <p class="no-current-image">No image uploaded</p>
+            @endisset
+          </div>
+        </div>
+        <div class="form-group">
+          <div data-component="FancyInput"
+               data-prop-input-name="{{ $role }}_image"
+               data-prop-initial-input-value="{{ isset($vip->bride->image) ?: $vip->bride->image }}"
+               data-prop-template="vip"
+          >
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xs-12">
+        {{-- Submit Button --}}
+        <div class="form-group">
+          @can('update', App\VIP::class)
+            {{ Form::submit('Update', ['class' => 'btn btn-success']) }}
+          @else
+            {{ Form::unauthorizedButton() }}
+          @endcan
+        </div>
+      </div>
+      {{ Form::close() }}
+    @endslot
+  @endcomponent
+@endsection
