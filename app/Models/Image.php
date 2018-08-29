c1a5d373fc809a26a817325f1957b0f55a65205e
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-08-30 00:18:09 +0800

    Relationships from Image model

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -3,20 +3,16 @@
 namespace App\Models;
 
 use App\Traits\UploadsImage;
-use App\Repositories\Albums;
 
 
 /**
  * App\Image
  *
  * @property int $id
- * @property string $file_name
- * @property string $url_asset
- * @property string $url_cache
+ * @property string $name
+ * @property string $url
  * @property int $featured
  * @property string|null $caption
- * @property int $imageable_id
- * @property string $imageable_type
  * @property \Carbon\Carbon|null $created_at
  * @property \Carbon\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $imageable
@@ -24,60 +20,47 @@ use App\Repositories\Albums;
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\ whereCaption($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\ whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\ whereFeatured($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\ whereFileName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Image whereName($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\ whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\ whereImageableId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\ whereImageableType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Image whereUrl($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\ whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\ whereUrlAsset($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\ whereUrlCache($value)
- *
  */
 class Image extends Model
 {
-	use UploadsImage;
+    use UploadsImage;
 
-	protected $albums;
-	
-	public function __construct(array $attributes = [])
-	{
-		parent::__construct($attributes);
-		$this->albums = new Albums;
-	}
-
-	public function imageable(){
-		return $this->morphTo();
-	}
-
-	public function album()
+    public function albums()
     {
-		if ($this->imageable instanceof Album){
-			return $this->imageable;
-		}
-		return null;
-	}
+        return $this->morphedByMany(Album::class, 'imageable');
+    }
+
+    public function bridesBests()
+    {
+        return $this->morphedByMany(BridesBest::class, 'imageable');
+    }
+
+    public function carousels()
+    {
+        return $this->morphedByMany(Carousel::class, 'imageable');
+    }
+
+    public function events()
+    {
+        return $this->morphedByMany(Event::class, 'imageable');
+    }
+
+    public function posts()
+    {
+        return $this->morphedByMany(Post::class, 'imageable');
+    }
 
     public function isFeatured()
     {
         return $this->featured === '*';
     }
 
-	/**
-	 * Assign image with given attributes to Uncategorized Album
-	 * 
-	 * @param  array $attributes
-	 * @return void
-	 * 
-	 */
-	public function assignToUncategorizedAlbum(array $attributes)
-	{
-		$this->albums->uncategorized()
-			   		->images()
-			   		->create($attributes);
-	}
-
-	public static function byName($name)
-	{
-		return static::where('file_name', $name)->firstOrFail();
-	}
+    public function urlCache($template = "original")
+    {
+        return url(config('imagecache.route') . "/" . $template . "/" . $this->name);
+    }
 }
